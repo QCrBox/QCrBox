460c9e894040111a130ff16fa9f01c48700a146e
Maximilian Albert <maximilian.albert@gmail.com>
2023-10-24 23:06:50 +0100

    Start mkdocs server in the background and wait for it to shut down when Ctrl+C is pressed

--- qcrbox/qcrbox/cli/docs/commands.py
+++ qcrbox/qcrbox/cli/docs/commands.py
@@ -1,6 +1,11 @@
+import atexit
+import shutil
+import signal
+import subprocess
+import webbrowser
+
 import click
 import doit.task
-import webbrowser
 
 from ..helpers import run_tasks
 
@@ -48,15 +53,30 @@ def serve(host, port):
     """
     Serve the documentation using the MkDocs development server.
     """
+    def build_and_serve_docs():
+        mkdocs_executable = shutil.which("mkdocs")
+        proc = subprocess.Popen([mkdocs_executable, "serve", "-a", dev_addr])
+        atexit.register(wait_for_mkdocs_server_shutdown, proc)
+
+    def wait_for_mkdocs_server_shutdown(proc):
+        print("Waiting for mkdocs server to shut down...")
+        proc.wait()
+        print("Done.")
+
+    def open_url(url):
+        webbrowser.open(url, new=2)
+
     dev_addr = f"{host}:{port}"
     url = f"http://{dev_addr}"
     task1 = doit.task.dict_to_task(
         {
-            "name": "serve-docs",
-            "actions": [f"mkdocs serve -a {dev_addr}"],
+            "name": "build-and-serve-docs",
+            "actions": [(build_and_serve_docs,)],
         }
     )
     task2 = doit.task.dict_to_task(
-        {"name": "open-browser", "actions": [webbrowser.open(url, new=2)]}
+        {"name": "open-browser", "actions": [(open_url, [url])],}
     )
     run_tasks([task1, task2])
+
+    signal.pause()
