70d3423aeac10ac47fbafdeca2c5797e5542fa21
Maximilian Albert <maximilian.albert@gmail.com>
2023-10-24 18:56:34 +0100

    Add subcommand to serve the docs using the MkDocs development server (incl. options to change the host/port)

--- qcrbox/qcrbox/cli/docs/commands.py
+++ qcrbox/qcrbox/cli/docs/commands.py
@@ -1,5 +1,6 @@
 import click
 import doit.task
+import webbrowser
 
 from ..helpers import run_tasks
 
@@ -24,3 +25,38 @@ def build():
         }
     )
     run_tasks([task])
+
+
+@docs_group.command()
+@click.option(
+    "-h",
+    "--host",
+    "host",
+    type=click.STRING,
+    default="localhost",
+    help="Host to serve documentation locally (default: localhost)",
+)
+@click.option(
+    "-p",
+    "--port",
+    "port",
+    type=click.INT,
+    default=8000,
+    help="Port to serve documentation locally (default: 8000)",
+)
+def serve(host, port):
+    """
+    Serve the documentation using the MkDocs development server.
+    """
+    dev_addr = f"{host}:{port}"
+    url = f"http://{dev_addr}"
+    task1 = doit.task.dict_to_task(
+        {
+            "name": "serve-docs",
+            "actions": [f"mkdocs serve -a {dev_addr}"],
+        }
+    )
+    task2 = doit.task.dict_to_task(
+        {"name": "open-browser", "actions": [webbrowser.open(url, new=2)]}
+    )
+    run_tasks([task1, task2])
