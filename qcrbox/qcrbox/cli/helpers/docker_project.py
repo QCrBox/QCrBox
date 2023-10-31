a09e3989b3448d3ce0bf879961894a19546024d4
Maximilian Albert <maximilian.albert@gmail.com>
2023-10-31 14:57:57 +0000

    Add helper methods to DockerProject to run a given docker command and build an image

--- qcrbox/qcrbox/cli/helpers/docker_project.py
+++ qcrbox/qcrbox/cli/helpers/docker_project.py
@@ -1,4 +1,9 @@
+import os
 import pathlib
+import shutil
+import subprocess
+import sys
+
 import yaml
 from pathlib import Path
 
@@ -6,7 +11,8 @@ from git.exc import InvalidGitRepositoryError
 from pydantic.v1.utils import deep_update
 from typing import TypeVar
 
-from .qcrbox_helpers import get_repo_root
+from .qcrbox_helpers import get_repo_root, get_current_qcrbox_version
+from ..logging import logger
 
 # Type alias
 PathLike = TypeVar("PathLike", str, pathlib.Path)
@@ -58,3 +64,36 @@ class DockerProject:
     @property
     def services(self):
         return list(self.full_data["services"].keys())
+
+    def _construct_docker_compose_command(self, cmd: str, *cmd_args: str):
+        env_dev_file = self.repo_root.joinpath(".env.dev")
+
+        cmd = (
+            [
+                "docker",
+                "compose",
+                f"--project-name={self.project_name}",
+                f"--env-file={env_dev_file.as_posix()}",
+            ]
+            + [f"--file={compose_file.as_posix()}" for compose_file in self.compose_files]
+            + [cmd]
+            + list(cmd_args)
+        )
+
+        return cmd
+
+    def run_docker_compose_command(self, cmd: str, *cmd_args: str, capture_output: bool = False, dry_run: bool = False):
+        full_cmd = self._construct_docker_compose_command(cmd, *cmd_args)
+        logger.debug(f"Running docker compose command: {' '.join(full_cmd)!r}")
+
+        custom_env = os.environ.copy()
+        custom_env["QCRBOX_PYTHON_PACKAGE_VERSION"] = get_current_qcrbox_version()
+        logger.debug(f"Current qcrbox version: {custom_env['QCRBOX_PYTHON_PACKAGE_VERSION']}")
+
+        if not dry_run:
+            proc = subprocess.run(full_cmd, env=custom_env, shell=False, check=False, capture_output=capture_output)
+            return proc
+
+    def build_single_docker_image(self, target_image: str, dry_run: bool, capture_output: bool = False):
+        logger.info(f"Building docker image: {target_image}")
+        self.run_docker_compose_command("build", target_image, dry_run=dry_run, capture_output=capture_output)
