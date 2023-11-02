f6f79bf2a47654eec967c1eed327b3348f882ca5
Maximilian Albert <maximilian.albert@gmail.com>
2023-11-02 12:43:49 +0000

    Remove --all flag for status subcommand

--- qcrbox/qcrbox/cli/subcommands/status/commands.py
+++ qcrbox/qcrbox/cli/subcommands/status/commands.py
@@ -2,16 +2,16 @@ from typing import Optional
 
 import click
 from ...helpers import make_task, run_tasks, print_command_help_string_and_exit, exit_with_msg
+from ...helpers.docker_project import DockerProject
 from ...helpers.docker_helpers import (
+    get_all_services,
     get_status_of_docker_service,
     get_toplevel_docker_compose_path,
-    get_all_services,
 )
 from ...logging import logger
 
 
 @click.command(name="status")
-@click.option("--all", "include_all_components", is_flag=True, default=False, help="Include all components.")
 @click.option(
     "-f",
     "--file",
@@ -21,22 +21,13 @@ from ...logging import logger
     help="Docker compose file to use.",
 )
 @click.argument("components", nargs=-1)
-def get_component_status(include_all_components: bool, compose_file: Optional[str], components: list[str]):
+def get_component_status(compose_file: Optional[str], components: list[str]):
     """
     Show status of QCrBox components.
     """
     compose_file = compose_file or get_toplevel_docker_compose_path()
-
-    if components == ():
-        if include_all_components:
-            components = get_all_services(compose_file)
-        else:
-            print_command_help_string_and_exit()
-    else:
-        if include_all_components:
-            component_list = ", ".join(repr(s) for s in components)
-            exit_with_msg(f"Cannot combine --all with explicit component names (here: {component_list})")
-
+    dp = DockerProject("qcrbox", compose_file)
+    components = components or dp.services
     tasks = [task_get_status_of_docker_service(component, compose_file) for component in components]
     run_tasks(tasks)
 
