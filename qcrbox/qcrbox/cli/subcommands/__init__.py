c11a989d7e7d196b4b905d4e156c407713fce46c
Maximilian Albert <maximilian.albert@gmail.com>
2023-11-03 10:34:36 +0000

    Remove subcommand 'status' for the time being

--- qcrbox/qcrbox/cli/subcommands/__init__.py
+++ qcrbox/qcrbox/cli/subcommands/__init__.py
@@ -2,5 +2,4 @@ from .build import build_components
 from .docs import docs_build_and_serve
 from .down import shut_down_components
 from .info import show_info
-from .status import get_component_status
 from .up import start_up_components
