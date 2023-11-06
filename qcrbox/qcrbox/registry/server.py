d094cd317129c3289a54101d423704f9cfa11697
Maximilian Albert <maximilian.albert@gmail.com>
2023-11-06 21:04:59 +0000

    Increase robustness of QCrBox registry startup by retrying the connection attempt if it fails initially

--- qcrbox/qcrbox/registry/server.py
+++ qcrbox/qcrbox/registry/server.py
@@ -1,14 +1,42 @@
+import contextlib
 import uvicorn
+from aiormq.exceptions import AMQPConnectionError
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
 from propan.fastapi import RabbitRouter
+from tenacity import retry, wait_fixed, stop_after_attempt, retry_if_exception_type
 
 from ..cli.helpers.qcrbox_helpers import get_rabbitmq_connection_url
 from .database import create_db_and_tables, seed_database
 
+
+def wrap_with_retry(orig_connect_func, *, wait_interval, max_attempt_number):
+    @retry(
+        reraise=True,
+        wait=wait_fixed(wait_interval),
+        stop=stop_after_attempt(max_attempt_number),
+        retry=retry_if_exception_type(AMQPConnectionError),
+    )
+    async def connect_with_retries(*args, **kwargs):
+        logger.debug("Attempting to establish connection to RabbitMQ.")
+        await orig_connect_func(*args, **kwargs)
+
+    return connect_with_retries
+
+
+class RabbitRouterWithConnectionRetries(RabbitRouter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.broker.connect = wrap_with_retry(
+            self.broker.connect,
+            wait_interval=3,
+            max_attempt_number=50,
+        )
+
+
 rabbitmq_url = get_rabbitmq_connection_url()
-router = RabbitRouter(rabbitmq_url)
+router = RabbitRouterWithConnectionRetries(rabbitmq_url)
 fastapi_app = FastAPI(lifespan=router.lifespan_context, logger=logger)
 fastapi_app.add_middleware(
     CORSMiddleware,
@@ -25,14 +53,17 @@ async def init_db(_: FastAPI):
     seed_database()
 
 
+@router.after_startup
+async def report_successful_startup(_: FastAPI):
+    logger.info("QCrBox registry startup was successful.")
+
+
 def main():
     logger.info("Starting QCrBox registry server")
     try:
         uvicorn.run("qcrbox.registry.server:fastapi_app", host="0.0.0.0", port=8000, reload=True)
     except Exception as exc:
-        logger.info("Line 3")
-        logger.info(f"{exc=}")
-        logger.info("Line 4")
+        logger.info(f"[DDD] {exc=}")
         raise
 
 
