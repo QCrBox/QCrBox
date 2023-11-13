f3cfef47a7dfc469d098e4457cfd2ef55801b2df
Maximilian Albert <maximilian.albert@gmail.com>
2023-11-13 20:56:56 +0000

    Allow passing the container_qcrbox_id to 'qcb invoke'

--- qcrbox/qcrbox/cli/subcommands/invoke/commands.py
+++ qcrbox/qcrbox/cli/subcommands/invoke/commands.py
@@ -12,12 +12,21 @@ from ....registry.helpers import get_qcrbox_registry_api_connection_url
 @click.command(name="invoke")
 @click.option("--command-id", type=int, required=True, help="Id of the command to be invoked.")
 @click.option(
+    "--container-qcrbox-id",
+    type=str,
+    help=(
+        "Value of the 'qcrbox_id' attribute of the container which should "
+        "execute the command (run 'qcb list containers' to get the qcrbox_id). "
+        "By default, the first available container is used)."
+    ),
+)
+@click.option(
     "--with-args",
     type=str,
     default=None,
     help="JSON representation of the command arguments (run 'qcb list commands' to get details)",
 )
-def invoke_command(command_id: int, with_args: Optional[str] = None):
+def invoke_command(command_id: int, container_qcrbox_id: Optional[str] = None, with_args: Optional[str] = None):
     """
     Invoke a registered command with given arguments.
     """
@@ -35,6 +44,7 @@ def invoke_command(command_id: int, with_args: Optional[str] = None):
         payload=msg_specs.QCrBoxCalculationCreate(
             command_id=command_id,
             arguments=with_args,
+            container_qcrbox_id=container_qcrbox_id,
         ),
     )
     click.echo("Sending command invocation request to QCrBox")
