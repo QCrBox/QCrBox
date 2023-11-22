41e6d40d543c86ade4529ba08d8f2853f0c21b9e
Maximilian Albert <maximilian.albert@gmail.com>
2023-11-22 20:53:24 +0000

    Remove legacy files in qcrbox/cli/

--- qcrbox/qcrbox/cli/cli.py
+++ qcrbox/qcrbox/cli/cli.py
@@ -12,7 +12,6 @@ def entry_point():
 
 entry_point.add_command(subcommands.docs_build_and_serve)
 entry_point.add_command(subcommands.build_components)
-entry_point.add_command(subcommands.show_info)
 entry_point.add_command(subcommands.start_up_components)
 entry_point.add_command(subcommands.shut_down_components)
 entry_point.add_command(subcommands.print_list_of_resources)
