b4ae1b67983425d392d8f18fdf6045c8955cba2e
Maximilian Albert <maximilian.albert@gmail.com>
2023-11-22 21:55:00 +0000

    Add handler for msg_specs.GetCalculationStatusDetails

---
+++ qcrbox/qcrbox/registry/client/messaging/calculation_status.py
@@ -0,0 +1,34 @@
+from loguru import logger
+
+from .msg_processing import process_message
+from ...msg_specs import sql_models
+from ... import msg_specs
+
+__all__ = []
+
+
+@process_message.register
+async def _(msg: msg_specs.GetCalculationStatusDetails, application) -> msg_specs.QCrBoxGenericResponse:
+    """
+    Retrieve status details for a given calculation.
+    """
+    logger.debug(f"Retrieving status details for calculation_id={msg.payload.calculation_id}")
+    try:
+        calculation = application._calculations[msg.payload.calculation_id]
+    except KeyError:
+        error_msg = f"No calculation found for calculation_id={msg.payload.calculation_id}"
+        logger.error(error_msg)
+        return msg_specs.QCrBoxGenericResponse(
+            response_to="get_calculation_status_details", status="error", msg=error_msg
+        )
+
+    status_details = sql_models.QCrBoxCalculationStatusDetails(
+        status=calculation.status.value,
+        details=calculation.status_details,
+    ).dict()
+
+    return msg_specs.QCrBoxGenericResponse(
+        response_to="get_calculation_status_details",
+        status="success",
+        payload=status_details,
+    )
