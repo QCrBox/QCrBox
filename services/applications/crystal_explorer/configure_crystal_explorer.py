0f7a4d7c73411c55eede32203d8ac35d2e2fc085
Maximilian Albert <maximilian.albert@gmail.com>
2023-12-04 23:20:48 +0000

    Add CrystalExplorer

---
+++ services/applications/crystal_explorer/configure_crystal_explorer.py
@@ -0,0 +1,8 @@
+from qcrbox.registry.client import QCrBoxRegistryClient, ExternalCommand, Param
+
+cmd_open_file_in_crystal_explorer = ExternalCommand("/usr/bin/CrystalExplorer", "--open", Param("filename"))
+
+client = QCrBoxRegistryClient()
+application = client.register_application("CrystalExplorer", version="21.5")
+application.register_external_command("open_file", cmd_open_file_in_crystal_explorer)
+client.run()
