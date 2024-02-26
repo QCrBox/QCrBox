ac17366f710e3d2059056aa4298fca540a8f231c
Paul Niklas Ruth <panikruth@gmail.com>
2024-02-26 16:00:24 +0000

    more linting

--- services/applications/xharpy_gpaw/prebuild__01_clone_XHARPy.py
+++ services/applications/xharpy_gpaw/prebuild__01_clone_XHARPy.py
@@ -1,11 +1,10 @@
 import subprocess
 from pathlib import Path
 
-TARGET_DIR = Path(__file__).absolute().parent / 'XHARPy'
+TARGET_DIR = Path(__file__).absolute().parent / "XHARPy"
 
 # If target dir exists, update repo to the latest version. Otherwise, do a fresh clone to the target dir.
 subprocess.call(
-    f'git -C "{TARGET_DIR}" pull || git clone --depth 1 https://github.com/Niolon/XHARPy.git "{TARGET_DIR}"',
-    shell=True
+    f'git -C "{TARGET_DIR}" pull || git clone --depth 1 https://github.com/Niolon/XHARPy.git "{TARGET_DIR}"', shell=True
 )
 
