33580f231ce7b53c4fcc4bf46d51a503bdb3e5cf
Paul Niklas Ruth <panikruth@gmail.com>
2024-02-26 14:42:56 +0000

    create_tsc is working

---
+++ services/applications/xharpy_gpaw/configure_xharpy-gpaw.py
@@ -0,0 +1,76 @@
+import subprocess
+import shutil
+import os
+from pathlib import Path
+
+from qcrbox.registry.client import QCrBoxRegistryClient
+from qcrboxtools.cif.file_converter.hkl import cif2hkl4
+from qcrboxtools.cif.cif2cif import cif_file_unified_yml_instr, cif_file_unify_split
+
+YAML_PATH = '/opt/qcrbox/config_xharpy-gpaw.yaml'
+
+client = QCrBoxRegistryClient()
+application = client.register_application(
+    "XHARPy-GPAW",
+    version="0.2.0",
+)
+
+def atom_form_fact_gpaw(
+    input_cif_path,
+    output_tsc_path,
+    functional,
+    gridspacing
+):
+    work_cif_path = Path(input_cif_path).parent / "work.cif"
+    cif_file_unified_yml_instr(input_cif_path, work_cif_path, YAML_PATH, 'atom_form_fact_gpaw')
+    subprocess.call([
+        "python", "-m", "xharpy.cli_tsc",
+        "--cif_name", str(work_cif_path),
+        "--tsc_name", str(output_tsc_path),
+        "--xc", str(functional),
+        #"--kpoints", Param("kpoints"),
+        "--gridspacing", str(gridspacing),
+        "--auto_default",
+    ])
+
+
+application.register_python_callable("atom_form_fact_gpaw", atom_form_fact_gpaw)
+
+def ha_refine(
+    input_cif_path: str,
+    output_cif_path: str,
+    functional: str,
+    gridspacing: float
+):
+    input_cif_path = Path(input_cif_path)
+    output_dir = Path("./xharpy_output")
+    if output_dir.exists():
+        shutil.rmtree(output_dir)
+    output_dir.mkdir()
+
+    cif2hkl4(input_cif_path, 0, "shelx.hkl")
+
+    work_cif_path = input_cif_path.parent / "work.cif"
+    cif_file_unified_yml_instr(input_cif_path, work_cif_path, YAML_PATH, 'ha_refine')
+
+    subprocess.call([
+        "python", "-m", "xharpy.cli_refine",
+        "--cif_name", work_cif_path,
+        "--cif_index", "0",
+        "--hkl_name", "./shelx.hkl",
+        "--lst_name", "./dummy.lst",
+        "--extinction", "none",
+        "--xc", functional,
+        "--gridspacing", gridspacing,
+        "--kpoints", "1", "1", "1",
+        "--mpi_cores", "auto",
+        "--output_folder", output_dir
+    ])
+
+    cif_file_unify_split(output_dir / 'xharpy.cif', output_cif_path, custom_categories=['iucr, shelx'])
+    shutil.rmtree(output_dir)
+    os.remove('shelx.hkl')
+
+application.register_python_callable("ha_refine", ha_refine)
+
+client.run()
