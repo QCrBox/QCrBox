6a908b3ea67e3a8cc1bbb8b63bd03eb4ffdf6960
Maximilian Albert <maximilian.albert@gmail.com>
2024-02-28 13:19:31 +0000

    Minimal tests for initialising ParameterSpecDB and CommandSpecDB

---
+++ pyqcrbox/tests/test_sql_models.py
@@ -0,0 +1,17 @@
+from pyqcrbox.msg_specs.sql_models import CommandSpecDB, ParameterSpecDB
+
+
+def test_initialise_parameters():
+    _ = ParameterSpecDB(name="xxx", dtype="int", required=True)
+    _ = ParameterSpecDB(name="yyy", dtype="str", description="Some other parameter", required=False)
+
+
+def test_initialise_command():
+    param_a = ParameterSpecDB(name="a", dtype="int", required=True)
+    param_b = ParameterSpecDB(name="b", dtype="str", description="Some other parameter", required=False)
+
+    _ = CommandSpecDB(
+        name="calc_hypotenuse",
+        description="Given the lengths of two legs of a right triangle, calculate the length of the hypotenuse",
+        parameters=[param_a, param_b],
+    )
