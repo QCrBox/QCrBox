e2ba304807d4689924e0711644720eeccddbb783
Maximilian Albert <maximilian.albert@gmail.com>
2024-03-07 12:28:23 +0000

    Add test that duplicate registration succeeds

--- pyqcrbox/tests/test_client_registration.py
+++ pyqcrbox/tests/test_client_registration.py
@@ -1,5 +1,3 @@
-import logging
-
 import pytest
 from anyio import create_task_group
 from faststream.rabbit import RabbitBroker, TestRabbitBroker
@@ -32,7 +30,7 @@ def sample_application_cfg():
 
 
 @pytest.mark.asyncio
-async def test_client_registration_during_startup(sample_application_cfg):
+async def test_client_registers_itself_with_server_during_startup(sample_application_cfg):
     private_queue_name = "qcrbox_rk_test_client_xyz"
     expected_registration_message = msg_specs.RegisterApplication(
         action="register_application",
@@ -44,12 +42,12 @@ async def test_client_registration_during_startup(sample_application_cfg):
 
     broker = RabbitBroker(graceful_timeout=10)
     async with TestRabbitBroker(broker, with_real=False):
-        server_app = create_server_faststream_app(broker, log_level=logging.DEBUG)
+        server_app = create_server_faststream_app(broker, log_level="DEBUG")
         client_app = create_client_faststream_app(
             broker,
             private_queue_name=private_queue_name,
             application_spec=sample_application_cfg,
-            log_level=logging.DEBUG,
+            log_level="DEBUG",
         )
 
         async with create_task_group() as tg:
@@ -59,3 +57,42 @@ async def test_client_registration_during_startup(sample_application_cfg):
             tg.start_soon(client_app.run, client_max_messages)
 
         server_app.on_qcrbox_registry.mock.assert_called_once_with(expected_registration_message)
+
+
+@pytest.mark.asyncio
+async def test_client_registration_succeeds_even_if_same_application_was_registered_before(sample_application_cfg):
+    private_queue_name = "qcrbox_rk_test_client_xyz"
+    expected_registration_message = msg_specs.RegisterApplication(
+        action="register_application",
+        payload=msg_specs.RegisterApplication.Payload(
+            application_config=sample_application_cfg,
+            routing_key__registry_to_application=private_queue_name,
+        ),
+    ).dict()
+
+    broker = RabbitBroker(graceful_timeout=10)
+    async with TestRabbitBroker(broker, with_real=False):
+        server_app = create_server_faststream_app(broker, log_level="DEBUG")
+        client_app_1 = create_client_faststream_app(
+            broker, private_queue_name=private_queue_name, application_spec=sample_application_cfg, log_level="DEBUG"
+        )
+        client_app_2 = create_client_faststream_app(
+            broker, private_queue_name=private_queue_name, application_spec=sample_application_cfg, log_level="DEBUG"
+        )
+
+        async with create_task_group() as tg:
+            server_max_messages = 2
+            client_1_max_messages = 0
+            client_2_max_messages = 0
+            tg.start_soon(server_app.run, server_max_messages)
+            tg.start_soon(client_app_1.run, client_1_max_messages)
+            tg.start_soon(client_app_2.run, client_2_max_messages)
+
+        # TODO: it would be nice to use `assert_has_calls()` here (see [1]),
+        #       but I ran into issues trying to get this to work so I'm using
+        #       the slightly less exact comparison below for now.
+        #          -- Max, 7th March 2024
+        #
+        #       [1] https://docs.python.org/3/library/unittest.mock.html#unittest.mock.Mock.assert_has_calls
+        server_app.on_qcrbox_registry.mock.assert_called_with(expected_registration_message)
+        assert server_app.on_qcrbox_registry.mock.call_count == 2
