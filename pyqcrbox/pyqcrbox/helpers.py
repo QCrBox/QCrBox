c66ce7137f7b68e306c05687ece51186553529b8
Maximilian Albert <maximilian.albert@gmail.com>
2024-03-14 13:57:48 +0000

    Add helper functions to generate random correlation ids and private routing keys

---
+++ pyqcrbox/pyqcrbox/helpers.py
@@ -0,0 +1,23 @@
+from uuid import uuid4
+
+from faststream import Logger, apply_types
+
+__all__ = ["generate_correlation_id", "generate_private_routing_key"]
+
+
+@apply_types
+def generate_private_routing_key(logger: Logger):
+    result = create_unique_id(prefix="qcrbox_rk_")
+    logger.debug(f"Generated private routing key: {result}")
+    return result
+
+
+@apply_types
+def generate_correlation_id(logger: Logger):
+    result = create_unique_id(prefix="qcrbox_corr_id_")
+    logger.debug(f"Generated random correlation id: {result}")
+    return result
+
+
+def create_unique_id(*, prefix=""):
+    return f"{prefix}0x{uuid4().hex}"
