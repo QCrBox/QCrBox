62fde41b2bbdca83c4d326c648ffc24f1d4e219c
Maximilian Albert <maximilian.albert@gmail.com>
2024-03-21 11:24:42 +0000

    Convenience methods on CommandExecutionDB to rerieve an instance and the client routing key from a correlation_id

--- pyqcrbox/pyqcrbox/sql_models/command_execution.py
+++ pyqcrbox/pyqcrbox/sql_models/command_execution.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import Any, Optional
 
-from sqlmodel import JSON, Column, Field, Relationship
+from sqlmodel import JSON, Column, Field, Relationship, UniqueConstraint, select
 
 from pyqcrbox.settings import settings
 
@@ -26,6 +26,7 @@ class CommandExecutionCreate(QCrBoxPydanticBaseModel):
 
 class CommandExecutionDB(QCrBoxBaseSQLModel, table=True):
     __tablename__ = "command_execution"
+    __table_args__ = (UniqueConstraint("correlation_id"),)
     __pydantic_model_cls__ = CommandExecutionCreate
 
     application_slug: str
@@ -44,15 +45,25 @@ class CommandExecutionDB(QCrBoxBaseSQLModel, table=True):
     )
 
     @classmethod
-    def from_pydantic_model(cls, model):
+    def from_pydantic_model(cls, model) -> "CommandExecutionDB":
         pydantic_model_cls = getattr(cls, "__pydantic_model_cls__")
         assert isinstance(model, pydantic_model_cls)
         data = model.model_dump(exclude={"command_spec_db"})
         return cls(**data)
 
-    def save_to_db(self):
+    def save_to_db(self) -> "CommandExecutionDB":
         with settings.db.get_session() as session:
             session.add(self)
             session.commit()
             session.refresh(self)
             return self
+
+    @classmethod
+    def load_from_db(cls, correlation_id: str) -> "CommandExecutionDB":
+        with settings.db.get_session() as session:
+            return session.exec(select(cls).where(cls.correlation_id == correlation_id)).one()
+
+    @classmethod
+    def get_client_routing_key(cls, correlation_id: str) -> str:
+        cmd_execution_db = cls.load_from_db(correlation_id)
+        return cmd_execution_db.private_routing_key
