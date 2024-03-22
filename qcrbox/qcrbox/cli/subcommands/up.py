d7e4d62b811889cd3e94d73567a6b9201be4c219
Maximilian Albert <maximilian.albert@gmail.com>
2024-03-22 18:55:16 +0000

    Rename argument: --rebuild-deps -> --build-deps

--- qcrbox/qcrbox/cli/subcommands/up.py
+++ qcrbox/qcrbox/cli/subcommands/up.py
@@ -9,10 +9,10 @@ from .build import populate_build_tasks
 
 @click.command(name="up")
 @click.option(
-    "--rebuild-deps/--no-rebuild-deps",
+    "--build-deps/--no-build-deps",
     is_flag=True,
     default=True,
-    help="Rebuild dependencies before starting up the given components.",
+    help="Also build any dependencies of the given components.",
 )
 @click.option(
     "-n",
@@ -22,7 +22,7 @@ from .build import populate_build_tasks
     help="Display actions that would be performed without actually doing anything.",
 )
 @click.argument("components", nargs=-1)
-def start_up_components(rebuild_deps: bool, dry_run: bool, components: list[str]):
+def start_up_components(build_deps: bool, dry_run: bool, components: list[str]):
     """
     Start up QCrBox components.
     """
@@ -30,7 +30,7 @@ def start_up_components(rebuild_deps: bool, dry_run: bool, components: list[str]
     components = components or docker_project.services_excluding_base_images
 
     build_tasks = []
-    if rebuild_deps:
+    if build_deps:
         build_tasks += populate_build_tasks(components, docker_project, with_deps=True, dry_run=dry_run)
 
     startup_task = doit.task.dict_to_task(
