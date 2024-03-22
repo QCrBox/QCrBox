36ec1ed6ad697d907002549fa8c9fed479e56a95
Maximilian Albert <maximilian.albert@gmail.com>
2024-03-22 19:11:16 +0000

    Add separate flags --build and --build-deps to 'qcb up'

--- qcrbox/qcrbox/cli/subcommands/up.py
+++ qcrbox/qcrbox/cli/subcommands/up.py
@@ -9,10 +9,18 @@ from .build import populate_build_tasks
 
 @click.command(name="up")
 @click.option(
+    "--build/--no-build",
+    is_flag=True,
+    default=True,
+    show_default=True,
+    help="(Re-)build components before starting them up.",
+)
+@click.option(
     "--build-deps/--no-build-deps",
     is_flag=True,
     default=True,
-    help="Also build any dependencies of the given components.",
+    show_default=True,
+    help="Also build any dependencies of the given components. This option implies --build.",
 )
 @click.option(
     "-n",
@@ -22,16 +30,19 @@ from .build import populate_build_tasks
     help="Display actions that would be performed without actually doing anything.",
 )
 @click.argument("components", nargs=-1)
-def start_up_components(build_deps: bool, dry_run: bool, components: list[str]):
+def start_up_components(build: bool, build_deps: bool, dry_run: bool, components: list[str]):
     """
     Start up QCrBox components.
     """
     docker_project = DockerProject()
     components = components or docker_project.services_excluding_base_images
 
+    if not build:
+        build_deps = False
+
     build_tasks = []
-    if build_deps:
-        build_tasks += populate_build_tasks(components, docker_project, with_deps=True, dry_run=dry_run)
+    if build or build_deps:
+        build_tasks += populate_build_tasks(components, docker_project, with_deps=build_deps, dry_run=dry_run)
 
     startup_task = doit.task.dict_to_task(
         {
