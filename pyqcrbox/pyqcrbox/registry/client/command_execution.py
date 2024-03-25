97326ac6b792f0cacfc4c75b8090291b3fa8873b
Maximilian Albert <maximilian.albert@gmail.com>
2024-03-25 14:17:32 +0000

    Add support for executing Python callables

--- pyqcrbox/pyqcrbox/registry/client/command_execution.py
+++ pyqcrbox/pyqcrbox/registry/client/command_execution.py
@@ -2,6 +2,7 @@ from pyqcrbox.sql_models import ApplicationSpecCreate
 from pyqcrbox.sql_models.command import ImplementedAs
 
 from .external_command import ExternalCommand
+from .python_callable import PythonCallable
 
 __all__ = ["instantiate_command_from_spec"]
 
@@ -10,5 +11,7 @@ def instantiate_command_from_spec(app_spec: ApplicationSpecCreate, command_name:
     cmd_spec = app_spec.get_command_spec(command_name)
     if cmd_spec.implemented_as == ImplementedAs.cli:
         return ExternalCommand(cmd_spec.call_pattern)
+    if cmd_spec.implemented_as == ImplementedAs.python_callable:
+        return PythonCallable(cmd_spec.call_pattern)
     else:
         raise NotImplementedError(f"Cannot instantiate non-CLI commands. (Got: {cmd_spec.implemented_as.value})")
---
+++ pyqcrbox/pyqcrbox/registry/client/python_callable.py
@@ -0,0 +1,34 @@
+# SPDX-License-Identifier: MPL-2.0
+
+import inspect
+import subprocess
+from concurrent.futures import ThreadPoolExecutor
+from typing import Callable
+
+from .calculation import PythonCallableCalculation
+
+
+class PythonCallable:
+    def __init__(self, fn: Callable):
+        assert inspect.isfunction(fn)
+        if inspect.iscoroutinefunction(fn):
+            raise TypeError("At present PythonCallable can only handle regular functions, not coroutine functions.")
+
+        self.fn = fn
+        self.signature = inspect.signature(fn)
+        self.parameter_names = list(self.signature.parameters.keys())
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__}: {self.fn.__name__}{self.signature!s}>"
+
+    async def execute_in_background(
+        self,
+        *args,
+        _stdin=None,
+        _stdout=subprocess.PIPE,
+        _stderr=subprocess.PIPE,
+        **kwargs,
+    ):
+        with ThreadPoolExecutor(max_workers=1) as executor:
+            future = executor.submit(self.fn, *args, **kwargs)
+        return PythonCallableCalculation(future)
