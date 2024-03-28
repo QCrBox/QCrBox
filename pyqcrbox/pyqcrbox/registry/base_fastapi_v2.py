15ddf42dc15f154855fa304df7c1f09ef2b69b17
Maximilian Albert <maximilian.albert@gmail.com>
2024-03-28 11:16:25 +0000

    Add main() function which runs uvicorn

--- pyqcrbox/pyqcrbox/registry/base_fastapi_v2.py
+++ pyqcrbox/pyqcrbox/registry/base_fastapi_v2.py
@@ -1,5 +1,7 @@
+import uvicorn
 from fastapi import Depends, FastAPI
 from faststream.rabbit.fastapi import Logger, RabbitRouter
+from loguru import logger
 from pydantic import BaseModel
 
 router = RabbitRouter("amqp://guest:guest@localhost:5672/")
@@ -27,3 +29,16 @@ async def hello_http():
 
 app = FastAPI(lifespan=router.lifespan_context)
 app.include_router(router)
+
+
+def main():
+    logger.info("Starting QCrBox registry server")
+    try:
+        uvicorn.run("pyqcrbox.registry.base_fastapi_v2:app", host="0.0.0.0", port=8007, reload=True)
+    except Exception as exc:
+        logger.error(f"[DDD] {exc=}")
+        raise
+
+
+if __name__ == "__main__":
+    main()
