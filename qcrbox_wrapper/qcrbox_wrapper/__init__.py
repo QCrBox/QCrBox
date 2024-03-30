248fb93c0b74cf0ecf4696caa4604ce5ba7e7b75
Paul Niklas Ruth <paul.n.ruth@durham.ac.uk>
2024-03-30 15:00:12 +0000

    Add basic outline

---
+++ qcrbox_wrapper/qcrbox_wrapper/__init__.py
@@ -0,0 +1,3 @@
+from .qcrbox_wrapper import QcrBoxWrapper, QCrBoxApplication, QCrBoxCalculation, QCrBoxCalculationStatus, QCrBoxParameter, QCrBoxPathHelper
+
+__name__ = ["QcrBoxWrapper", "QCrBoxApplication", "QCrBoxCalculation", "QCrBoxCalculationStatus", "QCrBoxParameter", "QCrBoxPathHelper"]
