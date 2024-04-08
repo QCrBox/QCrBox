7063e96da81252375ebadcd858e68642dfd6b81c
Paul Niklas Ruth <panikruth@gmail.com>
2024-04-08 15:30:19 +0100

    As only runtest at the moment, remove extra files and calls

--- tests/test_olex2.py
+++ tests/test_olex2.py
@@ -27,24 +27,3 @@ def test_refine_iam(tmp_path_helper):
     calc.wait_while_running(1.0)
 
     assert calc.status.status == "completed"
-
-
-# @pytest.mark.serial
-@with_temp_path_helper
-def test_refine_tsc(tmp_path_helper):
-    shutil.copy(FILEPATH / "refine_nonconv_nonHaniso.cif", tmp_path_helper.path_to_local("input.cif"))
-    shutil.copy(FILEPATH / "refine_allaniso.tscb", tmp_path_helper.path_to_local("input.tscb"))
-
-    qcrbox = QCrBoxWrapper(os.environ["QCRBOX_REGISTRY_HOST"], os.environ["QCRBOX_REGISTRY_PORT"])
-    olex2 = qcrbox.application_dict["Olex2 (Linux)"]
-
-    calc = olex2.refine_tsc(
-        input_cif_path=tmp_path_helper.path_to_qcrbox("input.cif"),
-        tsc_path=tmp_path_helper.path_to_qcrbox("input.tscb"),
-        ls_cycles="20",
-        weight_cycles="5",
-    )
-
-    calc.wait_while_running(1.0)
-
-    assert calc.status.status == "completed"
