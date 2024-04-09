45f8d784463361297d67c3ace641d19086588c32
Maximilian Albert <maximilian.albert@gmail.com>
2024-04-09 12:25:32 +0100

    Migrate 'qcb down' and 'qcb version' over to pyqcrbox

---
+++ pyqcrbox/pyqcrbox/cli/subcommands/version.py
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: MPL-2.0
+
+import click
+
+from ... import __version__
+
+
+@click.command(name="version")
+def print_qcrbox_version():
+    """
+    Print the qcrbox version.
+    """
+    click.echo(__version__)
