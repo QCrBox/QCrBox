7b0b9d12e1cc0ee0722f39a975734615de6e9e85
Maximilian Albert <maximilian.albert@gmail.com>
2024-04-09 12:23:18 +0100

    Define __version__ attribute

--- pyqcrbox/pyqcrbox/__init__.py
+++ pyqcrbox/pyqcrbox/__init__.py
@@ -1,1 +1,13 @@
+# SPDX-License-Identifier: MPL-2.0
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("pyqcrbox")
+except PackageNotFoundError:
+    raise RuntimeError("The 'pyqcrbox' package is not correctly installed. Please install it with pip.")
+
+# Import logging module here to ensure the loggers
+# are set up correctly before any commands are run.
+from . import logging
 from .settings import settings
