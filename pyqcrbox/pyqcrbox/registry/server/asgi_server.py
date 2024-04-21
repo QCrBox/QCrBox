3401155d7690f2555434c216fdf21507e74188d5
Maximilian Albert <maximilian.albert@gmail.com>
2024-04-21 20:27:24 +0100

    Add stub for ASGI server implementation and associated test

--- pyqcrbox/pyqcrbox/registry/server/asgi_server.py
+++ pyqcrbox/pyqcrbox/registry/server/asgi_server.py
@@ -1,15 +1,18 @@
-from typing import Any
-
-from litestar import Litestar, get
+from litestar import Litestar, MediaType, get
 
 __all__ = ["create_asgi_server"]
 
 
-@get("/")
-async def hello() -> dict[str, Any]:
-    return {"msg": "Hello from QCrBox!"}
+@get("/", media_type=MediaType.TEXT)
+async def hello() -> str:
+    return "Hello from QCrBox!"
+
+
+@get(path="/health-check", media_type=MediaType.TEXT)
+async def health_check() -> str:
+    return "healthy"
 
 
 def create_asgi_server(custom_lifespan) -> Litestar:
-    app = Litestar([hello], lifespan=[custom_lifespan])
+    app = Litestar([hello, health_check], lifespan=[custom_lifespan])
     return app
--- pyqcrbox/tests/test_server.py
+++ pyqcrbox/tests/test_server.py
@@ -1,13 +1,13 @@
 import pytest
-from aioresult import ResultCapture
-from anyio import create_task_group
-from httpx import AsyncClient
+from litestar.status_codes import HTTP_200_OK
+from litestar.testing import AsyncTestClient
 
 
+@pytest.mark.xfail(reason="Not checking for server response yet")
 @pytest.mark.anyio
-async def test_ping_server_via_rabbitmq(create_qcrbox_test_server):
+async def test_health_check_via_rabbitmq(create_qcrbox_test_server):
     test_server = create_qcrbox_test_server()
-    msg = {"action": "ping", "payload": {}}
+    msg = {"action": "health_check", "payload": {}}
 
     assert not test_server.get_mock_handler("qcrbox-registry").called
 
@@ -15,17 +15,34 @@ async def test_ping_server_via_rabbitmq(create_qcrbox_test_server):
         await test_server.publish("qcrbox-registry", msg)
 
     test_server.get_mock_handler("qcrbox-registry").assert_called_once_with(msg)
+    raise NotImplementedError("TODO: verify the server response")
 
 
 @pytest.mark.anyio
-async def test_ping_server_via_web_api(create_qcrbox_test_server):
+async def test_hello(create_qcrbox_test_server):
     test_server = create_qcrbox_test_server()
 
     async with test_server.run():
-        async with AsyncClient() as ac, create_task_group() as tg:
-            res = ResultCapture.start_soon(tg, ac.get, "http://127.0.0.1:8000/")
+        async with AsyncTestClient(app=test_server.asgi_server) as web_client:
+            response = await web_client.get("/")
+            assert response.status_code == HTTP_200_OK
+            assert response.text == "Hello from QCrBox!"
 
-        response = res.result()
-        assert response.status_code == 200
-        breakpoint()
-        assert response.json() == {"msg": "Hello from QCrBox!"}
+
+@pytest.mark.anyio
+async def test_health_check_via_web_api(create_qcrbox_test_server):
+    test_server = create_qcrbox_test_server()
+
+    async with test_server.run():
+        async with AsyncTestClient(app=test_server.asgi_server) as web_client:
+            response = await web_client.get("/health-check")
+            assert response.status_code == HTTP_200_OK
+            assert response.text == "healthy"
+        #
+        # async with AsyncClient() as ac, create_task_group() as tg:
+        #     res = ResultCapture.start_soon(tg, ac.get, "http://127.0.0.1:8000/")
+
+        # response = res.result()
+        # assert response.status_code == 200
+        # breakpoint()
+        # assert response.json() == {"msg": "Hello from QCrBox!"}
