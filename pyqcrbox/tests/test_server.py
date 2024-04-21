dab4d220282265bb1f1933575d17c2f91a9548dd
Maximilian Albert <maximilian.albert@gmail.com>
2024-04-21 20:10:03 +0100

    Add test to ping qcrbox server via web API

--- pyqcrbox/tests/test_server.py
+++ pyqcrbox/tests/test_server.py
@@ -1,8 +1,11 @@
 import pytest
+from aioresult import ResultCapture
+from anyio import create_task_group
+from httpx import AsyncClient
 
 
 @pytest.mark.anyio
-async def test_ping_server(create_qcrbox_test_server):
+async def test_ping_server_via_rabbitmq(create_qcrbox_test_server):
     test_server = create_qcrbox_test_server()
     msg = {"action": "ping", "payload": {}}
 
@@ -12,3 +15,17 @@ async def test_ping_server(create_qcrbox_test_server):
         await test_server.publish("qcrbox-registry", msg)
 
     test_server.get_mock_handler("qcrbox-registry").assert_called_once_with(msg)
+
+
+@pytest.mark.anyio
+async def test_ping_server_via_web_api(create_qcrbox_test_server):
+    test_server = create_qcrbox_test_server()
+
+    async with test_server.run():
+        async with AsyncClient() as ac, create_task_group() as tg:
+            res = ResultCapture.start_soon(tg, ac.get, "http://127.0.0.1:8000/")
+
+        response = res.result()
+        assert response.status_code == 200
+        breakpoint()
+        assert response.json() == {"msg": "Hello from QCrBox!"}
