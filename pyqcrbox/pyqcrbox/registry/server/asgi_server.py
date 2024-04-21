1d774c978a0e9b5c54064ebe6b54473670bf6c42
Maximilian Albert <maximilian.albert@gmail.com>
2024-04-21 20:40:14 +0100

    Enable OpenAPI schema generation

--- pyqcrbox/pyqcrbox/registry/server/asgi_server.py
+++ pyqcrbox/pyqcrbox/registry/server/asgi_server.py
@@ -1,9 +1,10 @@
 from litestar import Litestar, MediaType, get
+from litestar.openapi import OpenAPIConfig
 
 __all__ = ["create_asgi_server"]
 
 
-@get("/", media_type=MediaType.TEXT)
+@get("/", media_type=MediaType.TEXT, include_in_schema=False)
 async def hello() -> str:
     return "Hello from QCrBox!"
 
@@ -14,5 +15,9 @@ async def health_check() -> str:
 
 
 def create_asgi_server(custom_lifespan) -> Litestar:
-    app = Litestar([hello, health_check], lifespan=[custom_lifespan])
+    app = Litestar(
+        route_handlers=[hello, health_check],
+        lifespan=[custom_lifespan],
+        openapi_config=OpenAPIConfig(title="QCrBox API", version="0.0.1"),
+    )
     return app
