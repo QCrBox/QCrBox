b5c9c489705b66fa443044ed686421e05f56191f
Maximilian Albert <maximilian.albert@gmail.com>
2024-04-22 11:30:48 +0100

    Logger can be imported from pyqcrbox directly

--- pyqcrbox/pyqcrbox/__init__.py
+++ pyqcrbox/pyqcrbox/__init__.py
@@ -2,12 +2,14 @@
 
 from importlib.metadata import PackageNotFoundError, version
 
+__all__ = ["logger", "settings"]
+
 try:
     __version__ = version("pyqcrbox")
 except PackageNotFoundError:
     raise RuntimeError("The 'pyqcrbox' package is not correctly installed. Please install it with pip.")
 
-# Import logging module here to ensure the loggers
-# are set up correctly before any commands are run.
-from . import logging
+# Import logging module first to ensure the loggers
+# are set up correctly before anything else is run.
+from .logging import logger
 from .settings import settings
