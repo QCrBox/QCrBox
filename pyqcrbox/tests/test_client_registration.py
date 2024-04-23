1d446fbd3f1ee58f456fa627998084d29acee417
Maximilian Albert <maximilian.albert@gmail.com>
2024-04-23 22:03:21 +0100

    Avoid hard-coding RabbitMQ queue name in test (instead derive from settings)

--- pyqcrbox/tests/test_client_registration.py
+++ pyqcrbox/tests/test_client_registration.py
@@ -1,6 +1,6 @@
 import pytest
 
-from pyqcrbox import msg_specs
+from pyqcrbox import msg_specs, settings
 
 
 @pytest.mark.xfail(reason="Client startup not fully implemented yet")
@@ -10,6 +10,7 @@ async def test_client_registers_itself_with_server_during_startup(
     create_qcrbox_test_client,
     sample_application_spec,
 ):
+    routing_key_qcrbox_registry = settings.rabbitmq.routing_key_qcrbox_registry
     private_routing_key = "qcrbox_rk_test_client_xyz"
     expected_registration_message = msg_specs.RegisterApplication(
         action="register_application",
@@ -20,8 +21,8 @@ async def test_client_registers_itself_with_server_during_startup(
     ).dict()
     expected_server_response = {"response_to": "register_application", "status": "success"}
 
-    assert not test_server.get_mock_handler("qcrbox-registry").called
+    assert not test_server.get_mock_handler(routing_key_qcrbox_registry).called
 
     async with create_qcrbox_test_client(private_routing_key=private_routing_key) as test_client:
-        test_server.get_mock_handler("qcrbox-registry").assert_called_once_with(expected_registration_message)
+        test_server.get_mock_handler(routing_key_qcrbox_registry).assert_called_once_with(expected_registration_message)
         test_client.get_mock_handler(private_routing_key).assert_called_once_with(expected_server_response)
