2d3ddd55317b132729cede06c8ca43b3202eeb28
Maximilian Albert <maximilian.albert@gmail.com>
2024-04-29 12:05:42 +0100

    Add server-side test for application registration (incl. saving to the database)

--- pyqcrbox/tests/test_server.py
+++ pyqcrbox/tests/test_server.py
@@ -1,15 +1,18 @@
 import pytest
 from litestar.status_codes import HTTP_200_OK
+from sqlmodel import select
+
+from pyqcrbox import msg_specs, settings, sql_models
 
 
 @pytest.mark.anyio
-async def test_health_check_via_rabbitmq(test_server, rabbit_test_broker):
+async def test_health_check_via_rabbitmq(test_server, rabbit_test_broker, server_public_queue_name):
     msg = {"action": "health_check", "payload": {}}
-    assert not test_server.get_mock_handler("qcrbox-registry").called
+    assert not test_server.get_mock_handler(server_public_queue_name).called
 
-    response = await rabbit_test_broker.publish(msg, "qcrbox-registry", rpc=True)
+    response = await rabbit_test_broker.publish(msg, server_public_queue_name, rpc=True)
 
-    test_server.get_mock_handler("qcrbox-registry").assert_called_once_with(msg)
+    test_server.get_mock_handler(server_public_queue_name).assert_called_once_with(msg)
     assert response.status == "success"
     assert response.msg == "healthy"
     assert response.payload.dict() == {"health_status": "healthy"}
@@ -29,3 +32,37 @@ async def test_health_check_via_web_api(test_server):
         response = await web_client.get("/health-check")
         assert response.status_code == HTTP_200_OK
         assert response.text == "healthy"
+
+
+@pytest.mark.anyio
+async def test_register_application(test_server, rabbit_test_broker, server_public_queue_name, sample_application_spec):
+    spec_cls = sql_models.ApplicationSpecDB
+
+    msg = msg_specs.RegisterApplication(
+        action="register_application",
+        payload=msg_specs.PayloadForRegisterApplication(
+            application_spec=sample_application_spec,
+            private_routing_key="qcrbox_rk_private",
+        ),
+    )
+
+    # Ensure we're starting with an empty database
+    with settings.db.get_session() as session:
+        assert len(session.exec(select(spec_cls)).all()) == 0
+
+    # Send registration message to the server
+    response = await rabbit_test_broker.publish(msg, server_public_queue_name, rpc=True)
+
+    # Check that we received a successful response
+    assert response.status == "success"
+
+    # Check that the application spec was saved to the database
+    with settings.db.get_session() as session:
+        result = session.exec(
+            select(spec_cls).where(
+                spec_cls.slug == sample_application_spec.slug and spec_cls.version == sample_application_spec.slug
+            )
+        ).one()
+        assert result.name == sample_application_spec.name
+        assert result.slug == sample_application_spec.slug
+        assert result.version == sample_application_spec.version
