f2148617349478834a43b1fc1b287b465bb1cd7c
Maximilian Albert <maximilian.albert@gmail.com>
2024-04-30 21:09:58 +0100

    refactor: improve code readability

--- pyqcrbox/pyqcrbox/db_helpers.py
+++ pyqcrbox/pyqcrbox/db_helpers.py
@@ -1,19 +1,15 @@
 import sqlalchemy
-from sqlmodel import select
+from sqlmodel import and_, select
 
 from pyqcrbox import settings
 
 __all__ = ["get_one", "get_one_or_none"]
 
 
-def _build_where_clauses(model_cls, **kwargs):
-    return (getattr(model_cls, attr_name) == value for attr_name, value in kwargs.items())
-
-
 def get_one(model_cls, **kwargs):
     with settings.db.get_session() as session:
-        where_clauses = _build_where_clauses(model_cls, **kwargs)
-        result = session.exec(select(model_cls).where(*where_clauses)).one()
+        conditions = [getattr(model_cls, attr_name) == value for attr_name, value in kwargs.items()]
+        result = session.exec(select(model_cls).where(and_(*conditions))).one()
         return result
 
 
