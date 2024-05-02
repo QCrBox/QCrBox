e464851c9fe3985b4a263cd40a107610f5b6303a
Maximilian Albert <maximilian.albert@gmail.com>
2024-05-02 13:33:13 +0100

    Improve client test so that it passes for both real and mock RabbitMQ broker

--- pyqcrbox/tests/test_client.py
+++ pyqcrbox/tests/test_client.py
@@ -4,14 +4,20 @@ from pyqcrbox import msg_specs
 
 
 @pytest.mark.anyio
-async def test_health_check_via_rabbitmq(test_client, rabbit_test_broker):
+async def test_health_check_via_rabbitmq(test_client, rabbit_test_broker, using_mock_rabbitmq_broker):
     msg = {"action": "health_check", "payload": {}}
     expected_response = msg_specs.responses.health_check_healthy()
+    if not using_mock_rabbitmq_broker:
+        expected_response = expected_response.dict()
 
     private_routing_key = test_client.private_routing_key
-    assert not test_client.handler_was_called(private_routing_key)
+
+    if using_mock_rabbitmq_broker:
+        assert not test_client.handler_was_called(private_routing_key)
 
     response = await rabbit_test_broker.publish(msg, private_routing_key, rpc=True)
 
-    test_client.get_mock_handler(private_routing_key).assert_called_once_with(msg)
+    if using_mock_rabbitmq_broker:
+        test_client.get_mock_handler(private_routing_key).assert_called_once_with(msg)
+
     assert response == expected_response
