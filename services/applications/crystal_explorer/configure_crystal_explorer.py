8c5dfec8c257c37b5e86bf6b2eac98abb94242b6
Paul Niklas Ruth <panikruth@gmail.com>
2024-05-16 08:36:50 +0100

    Adapt Crystal Explorer to new YML format

--- services/applications/crystal_explorer/configure_crystal_explorer.py
+++ services/applications/crystal_explorer/configure_crystal_explorer.py
@@ -10,9 +10,9 @@ YAML_PATH = "./config_crystal_explorer.yaml"
 def prepare__interactive(input_cif_path):
     input_cif_path = Path(input_cif_path)
     work_folder = input_cif_path.parent
-    work_cif = work_folder / "work.cif"
+    work_cif = work_folder / "qcrbox_work.cif"
 
-    cif_file_to_specific_by_yml(input_cif_path, work_cif, YAML_PATH, "interactive")
+    cif_file_to_specific_by_yml(input_cif_path, work_cif, YAML_PATH, "interactive", "input_cif_path")
 
 
 cmd_open_file_in_crystal_explorer = ExternalCommand("/usr/bin/CrystalExplorer", "--open", Param("input_cif_path"))
