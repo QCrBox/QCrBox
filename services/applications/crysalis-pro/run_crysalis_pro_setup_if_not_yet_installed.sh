a3ed2d768ffa0e9dd913bc0db0e552ea827ac7a5
Paul Niklas Ruth <paul.n.ruth@durham.ac.uk>
2024-05-17 11:38:06 +0100

    Start CrysalisPro during container construction and finish dialog

--- services/applications/crysalis-pro/run_crysalis_pro_setup_if_not_yet_installed.sh
+++ services/applications/crysalis-pro/run_crysalis_pro_setup_if_not_yet_installed.sh
@@ -28,11 +28,36 @@ export DISPLAY=:99
 # Run CrysAlis Pro installer in silent mode
 wine ${NEW_CRYSALIS_PRO_INSTALLER} /S /v/qn
 
+NEW_CRYSALIS_PRO_EXECUTABLE=$(find ${WINEPREFIX}/drive_c/Xcalibur/CrysAlisPro* -name pro.exe -print -quit || true)
+
+wine ${NEW_CRYSALIS_PRO_EXECUTABLE} &
+
+until xdotool search --name "Choose*"
+do
+    echo "..."
+    sleep 1
+done
+
+# Choose "No" in the "Choose to set up for measurement" dialog
+xdotool search --name "Choose*" windowfocus
+xdotool key Alt+N
+
+until xdotool search --name "Open*"
+do
+    echo "..."
+    sleep 1
+done
+
+# Close the opened Crysalis Pro window
+xdotool search --name "Open*" windowfocus
+xdotool key Alt+F4
+# xdotool selectwindow windowkill
+
 unset DISPLAY
 echo "CrysAlis Pro setup script finished. Killing Xvfb..."
 killall Xvfb
 
-NEW_CRYSALIS_PRO_EXECUTABLE=$(find ${WINEPREFIX}/drive_c/Xcalibur/CrysAlisPro* -name pro.exe -print -quit || true)
+echo "Creating desktop shortcut for CrysAlisPro..."
 
 cat << EOF > ./.idesktop/crysalispro.lnk
 table Icon
