a4284b1503ee62f394325540ce08b6a8452afd06
Maximilian Albert <maximilian.albert@gmail.com>
2024-05-29 00:15:02 +0100

    Allow dicts in cif entry sets (needed to support alternative options via 'one_of')

--- pyqcrbox/pyqcrbox/sql_models/cif_entry_set.py
+++ pyqcrbox/pyqcrbox/sql_models/cif_entry_set.py
@@ -3,5 +3,5 @@ from .qcrbox_base_models import QCrBoxPydanticBaseModel
 
 class CifEntrySetCreate(QCrBoxPydanticBaseModel):
     name: str
-    required: list[str] = []
-    optional: list[str] = []
+    required: list[str | dict] = []  # TODO: validate the dict structure (see 'resolution' in config_xharpy_gpaw.yaml)
+    optional: list[str | dict] = []  # TODO: validate the dict structure (see 'resolution' in config_xharpy_gpaw.yaml)
