28b57668130cf2b1e017e95f02eba60a2dd81504
Maximilian Albert <maximilian.albert@gmail.com>
2024-05-31 13:10:50 +0100

    Add health-check endpoint to client API

--- pyqcrbox/pyqcrbox/registry/client/api.py
+++ pyqcrbox/pyqcrbox/registry/client/api.py
@@ -1,11 +1,16 @@
-from litestar import Litestar
+from litestar import Litestar, MediaType, get
 
 __all__ = ["create_client_asgi_server"]
 
 
+@get(path="/health-check", media_type=MediaType.TEXT)
+async def health_check() -> str:
+    return "healthy"
+
+
 def create_client_asgi_server(custom_lifespan) -> Litestar:
     app = Litestar(
-        route_handlers=[],
+        route_handlers=[health_check],
         lifespan=[custom_lifespan],
         openapi_config=None,
     )
