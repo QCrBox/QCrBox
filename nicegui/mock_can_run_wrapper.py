730d16dc3cd26797191a71f15aae3d3a6d493e47
Paul Niklas Ruth <panikruth@gmail.com>
2024-06-12 18:51:30 +0100

    Load files from disk / load results of execution

--- nicegui/mock_can_run_wrapper.py
+++ nicegui/mock_can_run_wrapper.py
@@ -1,66 +1,34 @@
 from qcrbox_wrapper import QCrBoxWrapper
 from qcrbox_wrapper.qcrbox_wrapper import QCrBoxCommand, QCrBoxCommandBase, QCrBoxInteractiveCommand
+from qcrboxtools.cif.cif2cif import can_run_command
 from pathlib import Path
 from typing import List
 from hashlib import md5
+import yaml
 
-can_run_lookup = {
-    ('COD Check', 'get_number_fitting_cod_entries', '9dff3be99f4bb4f5fcdbc1d3cf1a6ff6'): True,
-    ('COD Check', 'merge_closest_cod_entry', '9dff3be99f4bb4f5fcdbc1d3cf1a6ff6'): True,
-    ('CrysalisPro', 'interactive', '9dff3be99f4bb4f5fcdbc1d3cf1a6ff6'): True,
-    ('CrystalExplorer', 'interactive', '9dff3be99f4bb4f5fcdbc1d3cf1a6ff6'): False,
-    ('Eval1X', 'interactive', '9dff3be99f4bb4f5fcdbc1d3cf1a6ff6'): True,
-    ('Eval1X', 'integrate', '9dff3be99f4bb4f5fcdbc1d3cf1a6ff6'): True,
-    ('Olex2 (Linux)', 'interactive', '9dff3be99f4bb4f5fcdbc1d3cf1a6ff6'): True,
-    ('Olex2 (Linux)', 'refine_iam', '9dff3be99f4bb4f5fcdbc1d3cf1a6ff6'): False,
-    ('Olex2 (Linux)', 'refine_tsc', '9dff3be99f4bb4f5fcdbc1d3cf1a6ff6'): False,
-    ('Olex2 (Linux)', 'run_cmds_file', '9dff3be99f4bb4f5fcdbc1d3cf1a6ff6'): True,
-    ('QCrBoxTools', 'replace_structure_from_cif', '9dff3be99f4bb4f5fcdbc1d3cf1a6ff6'): True,
-    ('QCrBoxTools', 'check_structure_convergence', '9dff3be99f4bb4f5fcdbc1d3cf1a6ff6'): True,
-    ('QCrBoxTools', 'iso2aniso', '9dff3be99f4bb4f5fcdbc1d3cf1a6ff6'): False,
-    ('QCrBoxTools', 'to_unified_cif', '9dff3be99f4bb4f5fcdbc1d3cf1a6ff6'): True,
-    ('XHARPy-GPAW', 'atom_form_fact_gpaw', '9dff3be99f4bb4f5fcdbc1d3cf1a6ff6'): False,
-    ('XHARPy-GPAW', 'ha_refine', '9dff3be99f4bb4f5fcdbc1d3cf1a6ff6'): False,
-    ('COD Check', 'get_number_fitting_cod_entries', 'a40b800ab4e8e7d7a91cc5bb0cbc819c'): True,
-    ('COD Check', 'merge_closest_cod_entry', 'a40b800ab4e8e7d7a91cc5bb0cbc819c'): True,
-    ('CrysalisPro', 'interactive', 'a40b800ab4e8e7d7a91cc5bb0cbc819c'): True,
-    ('CrystalExplorer', 'interactive', 'a40b800ab4e8e7d7a91cc5bb0cbc819c'): True,
-    ('Eval1X', 'interactive', 'a40b800ab4e8e7d7a91cc5bb0cbc819c'): True,
-    ('Eval1X', 'integrate', 'a40b800ab4e8e7d7a91cc5bb0cbc819c'): True,
-    ('Olex2 (Linux)', 'interactive', 'a40b800ab4e8e7d7a91cc5bb0cbc819c'): True,
-    ('Olex2 (Linux)', 'refine_iam', 'a40b800ab4e8e7d7a91cc5bb0cbc819c'): True,
-    ('Olex2 (Linux)', 'refine_tsc', 'a40b800ab4e8e7d7a91cc5bb0cbc819c'): True,
-    ('Olex2 (Linux)', 'run_cmds_file', 'a40b800ab4e8e7d7a91cc5bb0cbc819c'): True,
-    ('QCrBoxTools', 'replace_structure_from_cif', 'a40b800ab4e8e7d7a91cc5bb0cbc819c'): True,
-    ('QCrBoxTools', 'check_structure_convergence', 'a40b800ab4e8e7d7a91cc5bb0cbc819c'): True,
-    ('QCrBoxTools', 'iso2aniso', 'a40b800ab4e8e7d7a91cc5bb0cbc819c'): True,
-    ('QCrBoxTools', 'to_unified_cif', 'a40b800ab4e8e7d7a91cc5bb0cbc819c'): True,
-    ('XHARPy-GPAW', 'atom_form_fact_gpaw', 'a40b800ab4e8e7d7a91cc5bb0cbc819c'): True,
-    ('XHARPy-GPAW', 'ha_refine', 'a40b800ab4e8e7d7a91cc5bb0cbc819c'): True,
-    ('COD Check', 'get_number_fitting_cod_entries', '207eaecc00fdbf2d034f8ff101f3b86a'): True,
-    ('COD Check', 'merge_closest_cod_entry', '207eaecc00fdbf2d034f8ff101f3b86a'): True,
-    ('CrysalisPro', 'interactive', '207eaecc00fdbf2d034f8ff101f3b86a'): True,
-    ('CrystalExplorer', 'interactive', '207eaecc00fdbf2d034f8ff101f3b86a'): True,
-    ('Eval1X', 'interactive', '207eaecc00fdbf2d034f8ff101f3b86a'): True,
-    ('Eval1X', 'integrate', '207eaecc00fdbf2d034f8ff101f3b86a'): True,
-    ('Olex2 (Linux)', 'interactive', '207eaecc00fdbf2d034f8ff101f3b86a'): True,
-    ('Olex2 (Linux)', 'refine_iam', '207eaecc00fdbf2d034f8ff101f3b86a'): True,
-    ('Olex2 (Linux)', 'refine_tsc', '207eaecc00fdbf2d034f8ff101f3b86a'): True,
-    ('Olex2 (Linux)', 'run_cmds_file', '207eaecc00fdbf2d034f8ff101f3b86a'): True,
-    ('QCrBoxTools', 'replace_structure_from_cif', '207eaecc00fdbf2d034f8ff101f3b86a'): True,
-    ('QCrBoxTools', 'check_structure_convergence', '207eaecc00fdbf2d034f8ff101f3b86a'): True,
-    ('QCrBoxTools', 'iso2aniso', '207eaecc00fdbf2d034f8ff101f3b86a'): True,
-    ('QCrBoxTools', 'to_unified_cif', '207eaecc00fdbf2d034f8ff101f3b86a'): True,
-    ('XHARPy-GPAW', 'atom_form_fact_gpaw', '207eaecc00fdbf2d034f8ff101f3b86a'): True,
-    ('XHARPy-GPAW', 'ha_refine', '207eaecc00fdbf2d034f8ff101f3b86a'): False
-}
+def get_yaml_locations():
+    translation = {
+        "Crystal Explorer": "CrystalExplorer",
+        "Eval14/15": "Eval1X",
+    }
+    basedir = Path(__file__).parents[1] / 'services' / 'applications'
+    yml_location_dict = {}
+    yml_pathes = list(basedir.glob('*/*.yaml'))
+    for yml_path in yml_pathes:
+        with yml_path.open() as fobj:
+            config = yaml.safe_load(fobj)
+        app_name = translation.get(config['name'], config['name'])
+        yml_location_dict[app_name] = yml_path
+
+    return yml_location_dict
+
+yml_location_dict = get_yaml_locations()
 
 def get_mock_command(base_command: QCrBoxCommand) -> QCrBoxCommand:
     class MockCommand(type(base_command)):
         def can_run(self, input_cif_path) -> bool:
-            cif_hash = md5(Path(input_cif_path).read_bytes()).hexdigest()
-            return can_run_lookup[(self.application_name, self.name, cif_hash)]
-
+            return can_run_command(yml_location_dict[self.application_name], self.name, input_cif_path)
+        
     if isinstance(base_command, QCrBoxCommand):
         return MockCommand(
             cmd_id=base_command.id,
--- nicegui/prototype3.py
+++ nicegui/prototype3.py
@@ -4,6 +4,9 @@ from nicegui import ui
 from textwrap import dedent
 import json
 import webbrowser
+from pathlib import Path
+import shutil
+#import time
 
 def load_textbox_values(command):
     try:
@@ -23,14 +26,15 @@ def load_textbox_values(command):
     return other_arguments
 
 def create_cif_paths(input_cif_path, command):
-    raw_path = pathhelper.path_to_qcrbox(f"{input_cif_path}")
+    #raw_path = pathhelper.path_to_qcrbox(f"{input_cif_path}")
+    blank_path = input_cif_path.with_suffix("")
     arguments = {}
     if "input_cif_path" in command.par_name_list:
-        arguments["input_cif_path"] = pathhelper.path_to_qcrbox(raw_path)
+        arguments["input_cif_path"] = input_cif_path
     if "output_cif_path" in command.par_name_list:
-        arguments["output_cif_path"] = raw_path.with_name(raw_path.name + "_output.cif")
+        arguments["output_cif_path"] = input_cif_path.with_name(blank_path.name + "_output.cif")
     if "work_cif_path" in command.par_name_list:
-        arguments["work_cif_path"] = raw_path.with_name(raw_path.name + "_work.cif")
+        arguments["work_cif_path"] = input_cif_path.with_name(blank_path.name + "_work.cif")
     return arguments
 
 class BaseCommandGuiRepresentation:
@@ -54,7 +58,7 @@ class BaseCommandGuiRepresentation:
 
 class CommandGuiRepresentation(BaseCommandGuiRepresentation):
     def execute_on_click(self):
-        arguments = create_cif_paths(select_dataset.value, self.command)
+        arguments = create_cif_paths(path_storage["qcrbox_path"], self.command)
 
         other_arguments = load_textbox_values(self.command)
 
@@ -69,6 +73,7 @@ class CommandGuiRepresentation(BaseCommandGuiRepresentation):
             ui.notify(f"Error: {e}")
             return
         ui.notify("Calculation finished")
+        load_cif_file()
 
 class InteractiveCommandGuiRepresentation(BaseCommandGuiRepresentation):
     started_run = False
@@ -84,7 +89,7 @@ class InteractiveCommandGuiRepresentation(BaseCommandGuiRepresentation):
             self.execute_button.set_text("Execute")
 
     def run(self):
-        arguments = create_cif_paths(select_dataset.value, self.command)
+        arguments = create_cif_paths(path_storage["qcrbox_path"], self.command)
 
         other_arguments = load_textbox_values(self.command)
 
@@ -112,16 +117,17 @@ class InteractiveCommandGuiRepresentation(BaseCommandGuiRepresentation):
             ui.notify(f"Error: {e}")
             return None
         self.finalised_run = True
+        load_cif_file()
 
-def select_dataset_changed(input_cif):
-    local_path = pathhelper.path_to_local(input_cif.value)
+
+def repopulate_grid():
     grid.clear()
     steering_commands = ("finalise__", "prepare__", "run__", "redo__", "toparams__")
     with grid:
         for command in qcrbox.commands:
             if any(command.name.startswith(val) for val in steering_commands):
                 continue
-            if not command.can_run(local_path):
+            if not command.can_run(path_storage["local_path"]):
                 continue
             if hasattr(command, "gui_url"):
                 representation = InteractiveCommandGuiRepresentation(command)
@@ -132,27 +138,62 @@ def select_dataset_changed(input_cif):
             representation.settings_template_button
             representation.execute_button
 
+def load_cif_file():
+    original_input = path_storage["local_path"]
+    blank_path = original_input.with_suffix("")
+    copy_cif_path = original_input.with_name(blank_path.name + "_output.cif")
+    local_folder_path, qcrbox_folder_path = pathhelper.create_next_step_folder()
+    local_path = local_folder_path / "input.cif"
+    qcrbox_path = qcrbox_folder_path / "input.cif"
 
-pathhelper = QCrBoxPathHelper.from_dotenv(".env.dev", "gui_folder/prototype3")
+    shutil.copy(copy_cif_path, local_path)
 
+    path_storage["local_path"] = local_path
+    path_storage["qcrbox_path"] = qcrbox_path
+
+    repopulate_grid()
+
+    location_label.set_text(f"The current input cif is at `{local_path}` from previous calculation")
+
+
+def upload_cif_file(uploader_event):
+    local_folder_path, qcrbox_folder_path = pathhelper.create_next_step_folder()
+    local_path = local_folder_path / "input.cif"
+    qcrbox_path = qcrbox_folder_path / "input.cif"
+
+    local_path.write_bytes(uploader_event.content.read())
+
+    path_storage["local_path"] = local_path
+    path_storage["qcrbox_path"] = qcrbox_path
+
+    repopulate_grid()
+
+    location_label.set_text(f"The current input cif is at `{local_path}` from file upload")
+
+#pathhelper = QCrBoxPathHelper.from_dotenv(".env.dev", "gui_folder/prototype3")
+
+pathhelper = QCrBoxPathHelper(Path(__file__).parents[1] / "shared_files", base_dir="gui_folder/prototype3")
+
+path_storage = {"local_path": None, "qcrbox_path": None}
 qcrbox = QCrBoxWrapper("127.0.0.1", 11000)
 
 cif_names = ["01_reduced.cif", "02_converged.cif", "03_notconverged.cif"]
 
 ui.markdown(
     dedent(
-        f"""
+        """
         # QCrBox GUI Prototype 3
 
         This is a prototype for developing the interaction of QCrBox with a non-GUI components.
-
-        The work directory is `{pathhelper.local_path}`
-
-        ## Select a dataset
-    """
+        """
     ).strip()
 )
-select_dataset = ui.select(cif_names, on_change=select_dataset_changed, value=cif_names[0])
+location_label = ui.label("Currently no cif file is loaded")
+#location_label = ui.label("The work directory is `{pathhelper.local_path}`")
+
+ui.markdown("## Upload a dataset")
+
+ui.upload(on_upload=upload_cif_file, auto_upload=True)
 
 ui.markdown("## Input settings in JSON format")
 
@@ -162,6 +203,6 @@ ui.markdown("## Available commands")
 
 grid = ui.grid(columns=4)
 
-select_dataset_changed(select_dataset)
+#select_dataset_changed(select_dataset)
 
 ui.run()
