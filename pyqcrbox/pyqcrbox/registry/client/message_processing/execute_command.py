634ba0cbc4280c4f66a9ba2b01760320341339b5
Maximilian Albert <maximilian.albert@gmail.com>
2024-06-15 16:02:36 +0100

    Add debugging message

--- pyqcrbox/pyqcrbox/registry/client/message_processing/execute_command.py
+++ pyqcrbox/pyqcrbox/registry/client/message_processing/execute_command.py
@@ -6,6 +6,7 @@ from .base_message_dispatcher import client_side_message_dispatcher
 @client_side_message_dispatcher.register
 async def handle_execute_command(msg: msg_specs.ExecuteCommand, *, self, **kwargs):
     assert msg.action == "execute_command"
+    logger.debug(f"About to execute command {msg.payload.command_name!r}")
 
     cmd = self.get_executable_command(msg.payload.command_name)
     calc = await cmd.execute_in_background(**msg.payload.arguments)
