186bbddce82b88ab2b682f9d2b894158780e2062
Maximilian Albert <maximilian.albert@gmail.com>
2024-07-01 10:35:45 +0000

    Revert "Disable logs for /healthcheck endpoint"
    This reverts commit 26339edae9b094f324d36b97bbaba53c51cd849e.

--- pyqcrbox/pyqcrbox/registry/client/api_endpoints.py
+++ pyqcrbox/pyqcrbox/registry/client/api_endpoints.py
@@ -1,16 +1,12 @@
 from litestar import Litestar, MediaType, get
-from litestar.middleware.logging import LoggingMiddlewareConfig
 from litestar.plugins.structlog import StructlogPlugin
 
 __all__ = ["create_client_asgi_server"]
 
 
-@get(path="/healthcheck", media_type=MediaType.TEXT, exclude_from_logs=True)
+@get(path="/healthcheck", media_type=MediaType.TEXT)
 async def health_check() -> str:
     return "healthy"
-
-
-logging_middleware_config = LoggingMiddlewareConfig(exclude_opt_key="exclude_from_logs")
 
 
 def create_client_asgi_server(custom_lifespan) -> Litestar:
@@ -18,7 +14,6 @@ def create_client_asgi_server(custom_lifespan) -> Litestar:
         route_handlers=[health_check],
         lifespan=[custom_lifespan],
         plugins=[StructlogPlugin()],
-        middleware=[logging_middleware_config.middleware],
         openapi_config=None,
     )
     return app
