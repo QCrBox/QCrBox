29d88d7e2d90da66fb76c1f84f912d8d73fbc0b3
Maximilian Albert <maximilian.albert@gmail.com>
2024-07-18 08:57:49 +0100

    Add CifEntrySet fields

--- pyqcrbox/pyqcrbox/sql_models_NEW_v2/cif_entry_set.py
+++ pyqcrbox/pyqcrbox/sql_models_NEW_v2/cif_entry_set.py
@@ -1,7 +1,19 @@
+from typing import Self
+
+from pydantic import model_validator
+
 from .base import QCrBoxPydanticBaseModel
 
 __all__ = ["CifEntrySet"]
 
 
 class CifEntrySet(QCrBoxPydanticBaseModel):
-    pass
+    name: str
+    required: list[str] | None = []
+    optional: list[str] | None = []
+
+    @model_validator(mode="after")
+    def ensure_required_and_optional_entries_are_not_both_missing(self) -> Self:
+        if self.required == [] and self.optional == []:
+            raise ValueError("At least one of 'required' and 'optional' entry sets must be provided.")
+        return self
