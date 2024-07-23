f0adca5f0d4e29d05f5229c5c3159d9fa96d288e
Maximilian Albert <maximilian.albert@gmail.com>
2024-07-23 13:46:04 +0100

    Flesh out ApplicationSpecDB

--- pyqcrbox/pyqcrbox/sql_models_NEW_v2/application_spec_db.py
+++ pyqcrbox/pyqcrbox/sql_models_NEW_v2/application_spec_db.py
@@ -1,7 +1,32 @@
-from sqlmodel import Field, SQLModel
+from datetime import datetime
+
+from sqlmodel import Field, Relationship, SQLModel
+
+from .application_spec import ApplicationSpec, ApplicationSpecBase
+from .command_spec import CommandSpecDB
 
 
-class ApplicationSpecDB(SQLModel, table=True):
+class ApplicationSpecDB(ApplicationSpecBase, SQLModel, table=True):
     __tablename__ = "application"
+    __pydantic_model_cls__ = ApplicationSpec
 
     id: int | None = Field(default=None, primary_key=True)
+    registered_at: datetime = Field(default_factory=datetime.now)
+    private_routing_key: str
+
+    commands: list[CommandSpecDB] = Relationship(back_populates="application")
+
+    def model_dump(self, **kwargs):
+        data = super().model_dump(**kwargs)
+        data["commands"] = [cmd.model_dump(**kwargs) for cmd in self.commands]
+        return data
+
+    @classmethod
+    def from_pydantic_model(cls, application: __pydantic_model_cls__, private_routing_key: str = None):
+        pydantic_model_cls = getattr(cls, "__pydantic_model_cls__")
+        assert isinstance(application, pydantic_model_cls)
+        data = application.model_dump(exclude={"commands"})
+        data["commands"] = [CommandSpecDB.from_pydantic_model(cmd) for cmd in application.commands]
+        data["private_routing_key"] = private_routing_key or "super-secret-private-routing-key-001"
+        # logger.debug(f"{command.name=}: {data=}")
+        return cls(**data)
--- pyqcrbox/pyqcrbox/sql_models_NEW_v2/command_spec/command_spec_db.py
+++ pyqcrbox/pyqcrbox/sql_models_NEW_v2/command_spec/command_spec_db.py
@@ -1,13 +1,22 @@
-from sqlmodel import Field, JSON
+from typing import TYPE_CHECKING, Any
+
+from sqlmodel import JSON, Field, Relationship, UniqueConstraint
 
 from ..base import QCrBoxBaseSQLModel
 from .base_command_spec import ImplementedAs
-from .call_pattern import CallPattern
-from ..parameter_spec import ParameterSpecDiscriminatedUnion
+from .command_spec import CommandSpec
+
 # from .interactive_command_spec import InteractiveLifecycleSteps, NonInteractiveCommandSpec
+
+if TYPE_CHECKING:
+    from pyqcrbox.sql_models_NEW_v2.application_spec_db import ApplicationSpecDB
 
 
 class CommandSpecDB(QCrBoxBaseSQLModel, table=True):
+    __tablename__ = "command"
+    __table_args__ = (UniqueConstraint("name", "application_id"),)
+    __pydantic_model_cls__ = CommandSpec
+
     name: str
     description: str = ""
     merge_cif_su: bool = False
@@ -16,7 +25,7 @@ class CommandSpecDB(QCrBoxBaseSQLModel, table=True):
 
     # for CLI commands
     call_pattern: str | None = None
-    parameters: list[dict] = Field(sa_type=JSON)
+    parameters: dict[Any, Any] = Field(sa_type=JSON)
 
     # for Python callables
     import_path: str | None = None
@@ -26,3 +35,17 @@ class CommandSpecDB(QCrBoxBaseSQLModel, table=True):
     # non_interactive_equivalent: NonInteractiveCommandSpec
 
     id: int | None = Field(primary_key=True, nullable=True)
+
+    application_id: int | None = Field(default=None, foreign_key="application.id")
+    application: "ApplicationSpecDB" = Relationship(back_populates="commands")
+
+    @classmethod
+    def from_pydantic_model(cls, command):
+        # pydantic_model_cls = getattr(cls, "__pydantic_model_cls__")
+        # assert isinstance(command, pydantic_model_cls)
+        # breakpoint()
+        data = command.model_dump(exclude={"parameters"})
+        # data["parameters"] = [ParameterSpecDB.from_pydantic_model(param) for param in command.parameters]
+        data["parameters"] = {param.name: param.model_dump() for param in command.parameters}
+        # logger.debug(f"{command.name=}: {data=}")
+        return cls(**data)
