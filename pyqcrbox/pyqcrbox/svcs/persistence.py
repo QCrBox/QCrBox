3501f4331e7dc5d301b4b201408578c2d4c36c1c
Maximilian Albert <maximilian.albert@gmail.com>
2024-07-23 22:47:36 +0100

    Add SQLitePersistenceAdapter

--- pyqcrbox/pyqcrbox/svcs/persistence.py
+++ pyqcrbox/pyqcrbox/svcs/persistence.py
@@ -3,6 +3,8 @@ from typing import TYPE_CHECKING
 
 import nats.js.errors
 
+from pyqcrbox.sql_models_NEW_v2 import ApplicationSpecDB
+
 from .helper_functions import get_nats_key_value
 
 if TYPE_CHECKING:
@@ -31,3 +33,9 @@ class NatsPersistenceAdapter(BasePersistenceAdapter):
 
         nats_value = application_spec.model_dump_json().encode()
         await kv_applications.put(nats_key, nats_value)
+
+
+class SQLitePersistenceAdapter(BasePersistenceAdapter):
+    async def save_application_spec(self, application_spec: "ApplicationSpec") -> None:
+        application_spec_db = ApplicationSpecDB.from_pydantic_model(application_spec)
+        application_spec_db.save_to_db()
