17981c3bbce774a66847ff1844a276885a1c9f46
Paul Niklas Ruth <panikruth@gmail.com>
2024-07-31 20:50:05 +0100

    Add required_entries and optional entries to cif spec. Add validation ensuring entries are present

--- pyqcrbox/pyqcrbox/sql_models_NEW_v2/parameter_spec/filesystem_path_parameters.py
+++ pyqcrbox/pyqcrbox/sql_models_NEW_v2/parameter_spec/filesystem_path_parameters.py
@@ -1,4 +1,6 @@
-from typing import Literal
+from typing import Literal, Self
+
+from pydantic import model_validator
 
 from .base_parameter_spec import BaseParameterSpec
 
@@ -28,10 +30,24 @@ class FolderPathParameterSpec(BaseFilesystemPathParameterSpec):
 
 
 class BaseCifFileParameterSpec(BaseFilesystemPathParameterSpec):
+    required_entries: list[str] = []
+    optional_entries: list[str] = []
     required_entry_sets: list[str] = []
     optional_entry_sets: list[str] = []
     merge_su: bool = False
     custom_categories: list[str] = []
+
+    @model_validator(mode="after")
+    def ensure_one_cif_entry_definition_present(self) -> Self:
+        cif_entry_sources = [
+            self.required_entries,
+            self.optional_entries,
+            self.required_entry_sets,
+            self.optional_entry_sets,
+        ]
+        if all(li == [] for li in cif_entry_sources):
+            raise ValueError("At least one of 'required' and 'optional' entry definitions or sets must be provided.")
+        return self
 
 
 class InputCifParameterSpec(BaseCifFileParameterSpec):
