a3bc643f1dfd79d791119ee2bbc53d60f68dc9ff
Paul Niklas Ruth <panikruth@gmail.com>
2024-07-31 13:35:20 +0100

    readd qcb init and update templates

--- services/applications/_templates/cli_application/{{cookiecutter.application_slug}}/configure_{{cookiecutter.application_slug}}.py
+++ services/applications/_templates/cli_application/{{cookiecutter.application_slug}}/configure_{{cookiecutter.application_slug}}.py
@@ -1,18 +1,38 @@
-from qcrbox.registry.client import ExternalCommand, QCrBoxRegistryClient
+from pathlib import Path
 
-client = QCrBoxRegistryClient()
-application = client.register_application(
-    "{{ cookiecutter.application_name }}",
-    version="{{ cookiecutter.application_version }}",
-)
+from qcrboxtools.cif.cif2cif import cif_file_merge_to_unified_by_yml, cif_file_to_specific_by_yml
 
-cmd_run_sample_script = ExternalCommand(
-    "/bin/bash",
-    "/opt/{{ cookiecutter.application_slug }}/sample_cmd.sh",
-)
-application.register_external_command(
-    "sample_cmd",
-    cmd_run_sample_script,
-)
+from pyqcrbox import sql_models_NEW_v2
+from pyqcrbox.registry.client import QCrBoxClient
 
-client.run()
+YAML_PATH = "./config_{{ cookiecutter.application_slug }}.yaml"
+
+def needed_as_function_name_not_sample_cmd_v2(
+        input_cif_path: Path,
+        output_cif_path: Path,
+        msg: str,
+        some_number: int
+    ):
+    input_cif_path = Path(input_cif_path)
+    output_cif_path = Path(output_cif_path)
+    work_cif_path = input_cif_path.with_name("qcrbox_work.cif")
+
+    cif_file_to_specific_by_yml(input_cif_path, work_cif_path, YAML_PATH, "sample_cmd_v2", "input_cif_path")
+
+    # Do something with the cif file here
+
+    cif_file_merge_to_unified_by_yml(
+        work_cif_path, output_cif_path,
+        input_cif_path, YAML_PATH,
+        "sample_cmd_v2",
+        "output_cif_path"
+    )
+
+
+if __name__ == "__main__":
+    application_spec = sql_models_NEW_v2.ApplicationSpec.from_yaml_file(
+        "config_{{ cookiecutter.application_slug }}.yaml"
+    )
+
+    client = QCrBoxClient(application_spec=application_spec)
+    client.run()
--- services/applications/_templates/cli_application/{{cookiecutter.application_slug}}/sample_cmd.sh
+++ services/applications/_templates/cli_application/{{cookiecutter.application_slug}}/sample_cmd.sh
@@ -2,6 +2,17 @@
 
 set -euo pipefail
 
+input_cif_path=$1
+output_cif_path=$2
+
+# Convert unified input cif to specific qcrbox_work.cif according to yml instructions
+# delete if there is no input cif
+python -m qcrboxtools.cif specific_by_yml $1 ./qcrbox_work.cif ./configure_{{ cookiecutter.application_slug }}.yml sample_cmd input_cif_path
+
+# Replace with running your application
 echo "Hello from '{{ cookiecutter.application_name }}' (version {{ cookiecutter.application_version }})"
-echo
-echo "TODO: accept some parameters and produce some output"
+
+# Convert specific output cif to unified output cif and merge with input according to yml
+# delete if there is no output cif
+python -m qcrboxtools.cif unified_by_yml ./qcrbox_work.cif $2 $1 ./configure_{{ cookiecutter.application_slug }}.yml sample_cmd output_cif_path
+
