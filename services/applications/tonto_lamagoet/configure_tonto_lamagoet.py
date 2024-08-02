8d7ba3664557fb048f5db0ca6c853f9039f37b67
Paul Niklas Ruth <panikruth@gmail.com>
2024-08-02 18:35:23 +0100

    Tonto starts in container. More (untested) work on python helper and yaml of tonto

--- services/applications/tonto_lamagoet/configure_tonto_lamagoet.py
+++ services/applications/tonto_lamagoet/configure_tonto_lamagoet.py
@@ -1,16 +1,17 @@
+import subprocess
 from pathlib import Path
 from textwrap import dedent
+
+from qcrboxtools.cif.cif2cif import cif_file_to_specific_by_yml
+from qcrboxtools.cif.file_converter.hkl import cif2hkl4
+from qcrboxtools.cif.read import read_cif_as_unified, read_cif_safe
 
 from pyqcrbox import sql_models_NEW_v2
 from pyqcrbox.registry.client import QCrBoxClient
 
-from qcrboxtools.cif.cif2cif import cif_file_merge_to_unified_by_yml, cif_file_to_specific_by_yml
-from qcrboxtools.cif.file_converter.hkl import cif2hkl4
-from qcrboxtools.cif.read import read_cif_as_unified, read_cif_safe, cifdata_str_or_index
-
 YAML_PATH = "./config_tonto_lamagoet.yaml"
 
-def run_hirshfeld_hf(input_cif_path, output_cif_path, multiplicity, charge, basis_name):
+def run_hirshfeld_hf(input_cif_path, output_cif_path, multiplicity, charge, basis_set):
     input_cif_path = Path(input_cif_path)
     work_cif_path = input_cif_path.with_name("work.cif")
 
@@ -36,6 +37,14 @@ def run_hirshfeld_hf(input_cif_path, output_cif_path, multiplicity, charge, basi
 
     _, data_name = read_cif_safe(work_cif_path, 0)
 
+    unified_block = read_cif_as_unified(work_cif_path, 0)
+    if "_refine_ls.extinction_coef" in unified_block:
+        optimise_extinction = "TRUE"
+    else:
+        optimise_extinction = "FALSE"
+
+    convergence = 0.001
+    convergence_tolerance = 0.001
     stdin = dedent(f"""\
         {{
         name= qcrbox
@@ -47,15 +56,15 @@ def run_hirshfeld_hf(input_cif_path, output_cif_path, multiplicity, charge, basi
         }}
         process_cif
         basis_directory= {work_cif_path.parent}
-        basis_name= {basis_name}
+        basis_name= {basis_set}
         crystal= {{
             xray_data= {{
-            refine_h_u_iso= %(refine_h_u_iso)s
-            refine_positions_only= %(refine_positions_only)s
-            thermal_smearing_model= %(thermal_smearing_model)s
-            partition_model= %(partition_model)s
-            optimise_extinction= %(optimise_extinction)s
-            correct_dispersion = %(correct_dispersion)s
+            refine_h_u_iso= TRUE
+            refine_positions_only= FALSE
+            thermal_smearing_model= hirshfeld
+            partition_model= gaussian
+            optimise_extinction= {optimise_extinction}
+            correct_dispersion = TRUE
             REDIRECT {hkl_file_path.name}
             }}
         }}
@@ -66,8 +75,8 @@ def run_hirshfeld_hf(input_cif_path, output_cif_path, multiplicity, charge, basi
         scfdata= {{
             kind = rhf
             initial_density= promolecule
-            convergence= %(convergence)s
-            diis= {{ convergence_tolerance= %(convergence_tolerance)s }}
+            convergence= {convergence}
+            diis= {{ convergence_tolerance= {convergence_tolerance} }}
         }}
         scf ! << do this
 
@@ -77,13 +86,15 @@ def run_hirshfeld_hf(input_cif_path, output_cif_path, multiplicity, charge, basi
             initial_density= restricted
             use_SC_cluster_charges= TRUE
             cluster_radius= 8 angstrom
-            convergence= %(convergence)s
-            diis= {{ convergence_tolerance= %(convergence_tolerance)s }}
+            convergence= {convergence}
+            diis= {{ convergence_tolerance= {convergence_tolerance} }}
         }}
 
         ! Do the refinement ...
         ! It repeatedly does SCF calculations
         refine_hirshfeld_atoms
+
+        put_CIF
 
         ! Clean up
         delete_scf_archives
@@ -91,6 +102,8 @@ def run_hirshfeld_hf(input_cif_path, output_cif_path, multiplicity, charge, basi
     """)
 
     work_cif_path.with_name('stdin').write_text(stdin, encoding="UTF-8")
+
+    subprocess.check_call(['tonto', '--b /opt/qcrbox/tonto/basis_sets'], cwd=work_cif_path.parent)
 
 
 
