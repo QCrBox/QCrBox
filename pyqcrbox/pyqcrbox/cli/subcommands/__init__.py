2d318b34a0ece521edda83c53cb4c1e1e53b87c6
Maximilian Albert <maximilian.albert@gmail.com>
2024-08-02 12:06:36 +0100

    Add 'qcb init' subcommand

--- pyqcrbox/pyqcrbox/cli/subcommands/__init__.py
+++ pyqcrbox/pyqcrbox/cli/subcommands/__init__.py
@@ -1,4 +1,4 @@
 # SPDX-License-Identifier: MPL-2.0
 
 # from . import build, docs, down, init, invoke, list, up, version
-from . import build, dev, docs, down, list, up, version
+from . import build, dev, docs, down, init, list, up, version
