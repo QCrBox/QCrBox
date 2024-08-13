1411a6e2c709c9afc6d86aef32e473d1e58e4c76
Maximilian Albert <maximilian.albert@gmail.com>
2024-08-13 22:48:58 +0100

    Add InteractiveCommand.execute_in_background()

--- pyqcrbox/pyqcrbox/registry/client/executable_command/interactive_command.py
+++ pyqcrbox/pyqcrbox/registry/client/executable_command/interactive_command.py
@@ -3,6 +3,20 @@ from pyqcrbox.registry.client.executable_command import BaseCommand
 __all__ = ["InteractiveCommand"]
 
 
+class InteractiveCmdCalculation:
+    pass
+
+
 class InteractiveCommand(BaseCommand):
+    async def execute_in_background(
+        self,
+        _calculation_id: str,
+        _stdin=None,
+        _stdout=None,
+        _stderr=None,
+        **kwargs,
+    ) -> InteractiveCmdCalculation:
+        pass
+
     def terminate(self):
         raise NotImplementedError("TODO: implement terminate() for interactive commands")
