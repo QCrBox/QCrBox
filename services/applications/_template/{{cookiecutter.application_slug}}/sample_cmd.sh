c2c7c93ae745864611f5b3170c029ea6d5b45149
Maximilian Albert <maximilian.albert@gmail.com>
2024-08-15 07:59:59 +0100

    Fix debugging message

--- services/applications/_template/{{cookiecutter.application_slug}}/sample_cmd.sh
+++ services/applications/_template/{{cookiecutter.application_slug}}/sample_cmd.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 echo "Hello from '{{ cookiecutter.application_name }}' (version {{ cookiecutter.application_version }})"
 echo
-echo "This script was invoked with the following arguments: @*"
+echo "This script was invoked with the following arguments: $*"
