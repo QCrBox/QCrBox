c872b7367f16b842a045e8fb82c27a8aecb75993
Paul Niklas Ruth <panikruth@gmail.com>
2024-08-22 16:12:40 +0100

    Add support for one_of in required_entries and optional_entries

--- pyqcrbox/pyqcrbox/sql_models_NEW_v2/parameter_spec/filesystem_path_parameters.py
+++ pyqcrbox/pyqcrbox/sql_models_NEW_v2/parameter_spec/filesystem_path_parameters.py
@@ -1,6 +1,8 @@
 from typing import Literal
 
+from pydantic import validator
 from .base_parameter_spec import BaseParameterSpec
+from ..base import QCrBoxPydanticBaseModel
 
 __all__ = [
     "FolderPathParameterSpec",
@@ -26,15 +28,23 @@ class GenericOutputFileParameterSpec(BaseFilesystemPathParameterSpec):
 class FolderPathParameterSpec(BaseFilesystemPathParameterSpec):
     dtype: Literal["QCrBox.folder_path"]
 
+class OneOfCifEntrySpec(QCrBoxPydanticBaseModel):
+    one_of: list[str|list[str]]
 
 class BaseCifFileParameterSpec(BaseFilesystemPathParameterSpec):
-    required_entries: list[str] = []
-    optional_entries: list[str] = []
+    required_entries: list[str|OneOfCifEntrySpec] = []
+    optional_entries: list[str|OneOfCifEntrySpec] = []
     required_entry_sets: list[str] = []
     optional_entry_sets: list[str] = []
     merge_su: bool = False
     custom_categories: list[str] = []
 
+    #TODO: Check this and add tests, add oneOfCifEntrySpec to entry sets?
+    @validator('required_entries', 'optional_entries', pre=True)
+    def parse_entries(cls, v):
+        if isinstance(v, list):
+            return [OneOfCifEntrySpec(one_of=item['one_of']) if isinstance(item, dict) and 'one_of' in item else item for item in v]
+        return v
 
 class InputCifParameterSpec(BaseCifFileParameterSpec):
     dtype: Literal["QCrBox.input_cif"]
