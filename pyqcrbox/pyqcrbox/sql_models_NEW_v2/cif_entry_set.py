40779fcc755508040e5c16692f9e45dcb946ae44
Maximilian Albert <maximilian.albert@gmail.com>
2024-08-23 05:23:41 +0100

    Add support for alternative cif enry options using 'one_of'

--- pyqcrbox/pyqcrbox/sql_models_NEW_v2/cif_entry_set.py
+++ pyqcrbox/pyqcrbox/sql_models_NEW_v2/cif_entry_set.py
@@ -1,16 +1,22 @@
-from typing import Self
+from typing import Literal, Self, Union
 
 from pydantic import model_validator
+from typing_extensions import TypeAliasType
 
 from .base import QCrBoxPydanticBaseModel
 
 __all__ = ["CifEntrySet"]
 
 
+CifEntryLiteral = TypeAliasType("CifEntryLiteral", str)
+CifEntryOneOf = TypeAliasType("CifEntryOneOf", dict[Literal["one_of"], list["CifEntryLiteral"]])
+CifEntry = TypeAliasType("CifEntry", Union[CifEntryLiteral, CifEntryOneOf])
+
+
 class CifEntrySet(QCrBoxPydanticBaseModel):
     name: str
-    required: list[str] | None = []
-    optional: list[str] | None = []
+    required: list[CifEntry] | None = []
+    optional: list[CifEntry] | None = []
 
     @model_validator(mode="after")
     def ensure_required_and_optional_entries_are_not_both_missing(self) -> Self:
