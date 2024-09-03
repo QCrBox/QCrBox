0b83377dd3fd8361d02893730598a31df95a7c1f
Maximilian Albert <maximilian.albert@gmail.com>
2024-09-03 21:58:20 +0100

    Add dummy_cli application

---
+++ services/applications/dummy_cli/configure_dummy_cli.py
@@ -0,0 +1,7 @@
+from pyqcrbox.registry.client import QCrBoxClient
+from pyqcrbox.sql_models_NEW_v2 import ApplicationSpec
+
+if __name__ == "__main__":
+    application_spec = ApplicationSpec.from_yaml_file("config_dummy_cli.yaml")
+    client = QCrBoxClient(application_spec=application_spec)
+    client.run()
---
+++ services/applications/dummy_cli/sample_cmd.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "Hello from 'Dummy CLI' (version 0.1.0)"
+echo
+echo "This script was invoked with the following arguments: $*"
