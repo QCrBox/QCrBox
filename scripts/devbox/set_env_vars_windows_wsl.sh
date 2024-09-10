531aef7b62684bea1c9880e340b4cc437d34aad4
Maximilian Albert <maximilian.albert@gmail.com>
2024-09-10 10:04:30 +0000

    Remove 'set -euo pipefail' from devbox scripts to avoid bash exiting upon tab completion etc.

--- scripts/devbox/set_env_vars_windows_wsl.sh
+++ scripts/devbox/set_env_vars_windows_wsl.sh
@@ -1,5 +1,3 @@
 export NIX_LD_LIBRARY_PATH=${PWD}/.devbox/nix/profile/default/lib/:${NIX_LD_LIBRARY_PATH:-}
 export LD_LIBRARY_PATH=${PWD}/.devbox/nix/profile/default/lib/:${LD_LIBRARY_PATH:-}
 export BROWSER=wslview  # Use wslview to open URLs in the default browser
-
-set -euo pipefail
