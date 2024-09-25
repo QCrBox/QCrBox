a6abd1bcfb3225607f5d7403bcc61924d11a7a55
Maximilian Albert <maximilian.albert@gmail.com>
2024-09-25 14:59:26 +0100

    Move retrieve_commands() implementation into api_helpers.py

--- pyqcrbox/pyqcrbox/registry/server/api/api_endpoints.py
+++ pyqcrbox/pyqcrbox/registry/server/api/api_endpoints.py
@@ -52,23 +52,7 @@ async def retrieve_applications(
 async def retrieve_commands(
     # name: str | None, application_slug: str | None, application_version: str | None
 ) -> list[sql_models.CommandSpecWithParameters]:
-    # model_cls = sql_models.CommandSpecDB
-    # filter_clauses = [
-    #     (name is None) or (model_cls.name == name),
-    #     (application_slug is None) or (sql_models.ApplicationSpecDB.slug == application_slug),
-    #     (application_version is None) or (sql_models.ApplicationSpecDB.version == application_version),
-    # ]
-    stmt = select(
-        sql_models.CommandSpecDB, sql_models.ApplicationSpecDB.slug, sql_models.ApplicationSpecDB.version
-    ).join(
-        sql_models.CommandSpecDB.application,
-    )
-
-    with settings.db.get_session() as session:
-        # commands = session.scalars(select(model_cls).where(*filter_clauses)).all()
-        commands = session.scalars(stmt).all()
-        commands = [cmd.to_response_model() for cmd in commands]
-        return commands
+    return api_helpers._retrieve_commands()
 
 
 @get(path="/calculations", media_type=MediaType.JSON)
--- pyqcrbox/pyqcrbox/registry/server/api/api_helpers.py
+++ pyqcrbox/pyqcrbox/registry/server/api/api_helpers.py
@@ -16,3 +16,27 @@ def _retrieve_applications() -> list[sql_models.ApplicationSpecWithCommands]:
         applications_response_models = [app.to_response_model() for app in applications]
 
     return applications_response_models
+
+
+def _retrieve_commands() -> list[sql_models.CommandSpecWithParameters]:
+    """
+    Retrieves list of commands from the database.
+    """
+    # model_cls = sql_models.CommandSpecDB
+    # filter_clauses = [
+    #     (name is None) or (model_cls.name == name),
+    #     (application_slug is None) or (sql_models.ApplicationSpecDB.slug == application_slug),
+    #     (application_version is None) or (sql_models.ApplicationSpecDB.version == application_version),
+    # ]
+    stmt = select(
+        sql_models.CommandSpecDB, sql_models.ApplicationSpecDB.slug, sql_models.ApplicationSpecDB.version
+    ).join(
+        sql_models.CommandSpecDB.application,
+    )
+
+    with settings.db.get_session() as session:
+        # commands = session.scalars(select(model_cls).where(*filter_clauses)).all()
+        commands = session.scalars(stmt).all()
+        commands = [cmd.to_response_model() for cmd in commands]
+
+    return commands
