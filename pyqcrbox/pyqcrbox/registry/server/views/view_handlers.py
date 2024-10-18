15190fa96358b10a987aba18865cabd442754064
Maximilian Albert <maximilian.albert@gmail.com>
2024-10-18 14:45:25 +0100

    WIP: Snapshot

--- pyqcrbox/pyqcrbox/registry/server/views/view_handlers.py
+++ pyqcrbox/pyqcrbox/registry/server/views/view_handlers.py
@@ -22,18 +22,42 @@ def render(*args, **kwargs) -> Response:
     return Response(content=rendered_content, media_type=MediaType.HTML)
 
 
+class WorkflowStep:
+    def __init__(self, *, workflow_id: int, step_id: int):
+        self.step_id = step_id
+        self.workflow_id = workflow_id
+        self.is_empty = True
+
+    @property
+    def html_div_id(self):
+        return f"workflow-{self.workflow_id}-step-{self.step_id}"
+
+
+class Workflow:
+    def __init__(self, *, title: str, steps: list[WorkflowStep], workflow_id: int):
+        self.workflow_id = workflow_id
+        self.title = title
+        self.steps = steps
+
+
 @get(path="/index", media_type=MediaType.HTML)
 async def serve_qcrbox_homepage() -> Response:
-    return render("QCrBoxHomePage")
+    workflow_id = 1
+    workflow_steps = [
+        # WorkflowStep(workflow_id=workflow_id, step_id=1),
+        # WorkflowStep(workflow_id=workflow_id, step_id=2),
+    ]
+    dummy_workflow = Workflow(title="Dummy Workflow", steps=workflow_steps, workflow_id=workflow_id)
+    return render("QCrBoxHomePage", workflow=dummy_workflow)
 
 
-@get(path="/workflow/{workflow_id:int}", media_type=MediaType.HTML)
+@get(path="/workflows/{workflow_id:int}", media_type=MediaType.HTML)
 async def serve_workflow_page(workflow_id: int) -> Response:
-    workflow_items = []
+    workflow_steps = []
     return render(
         "WorkflowPage",
         workflow_id=workflow_id,
-        items=workflow_items,
+        steps=workflow_steps,
     )
 
 
@@ -67,6 +91,20 @@ async def handle_data_file_upload(
     return render("DataFilesList", data_files=await api_helpers._get_data_files())
 
 
+@post(path="/workflows/{workflow_id:int}/initial_dataset")
+async def set_workflow_initial_dataset(workflow_id: int, qcrbox_data_file_id: str) -> Response:
+    from pyqcrbox import logger
+
+    logger.error(f"TODO: set initial dataset for workflow {workflow_id}")
+    workflow_step = WorkflowStep(workflow_id=workflow_id, step_id=1)
+    return render("WorkflowStep", step=workflow_step)
+
+
+# @post(path="/workflows/<workflow_id:int>/steps/<workflow_step_id:int>")
+# async def handle_workflow_step_update(workflow_id: int, workflow_step_id: int, qcrbox_data_file_id: str) -> Response:
+#     return render("WorkflowStep", item=None)
+
+
 views_router = Router(
     path="/views",
     route_handlers=[
@@ -75,6 +113,7 @@ views_router = Router(
         serve_data_files_page,
         serve_workflow_page,
         handle_data_file_upload,
+        set_workflow_initial_dataset,
         get_command_details,
     ],
 )
