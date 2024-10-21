21518e25c9bca1403bff884786f6aa03564efc18
Maximilian Albert <maximilian.albert@gmail.com>
2024-10-21 12:53:16 +0100

    Add QCrBoxDataset, QCrBoxDatasetResponse models

--- pyqcrbox/pyqcrbox/data_management/data_file.py
+++ pyqcrbox/pyqcrbox/data_management/data_file.py
@@ -16,3 +16,19 @@ class QCrBoxDataFile(BaseModel):
 class QCrBoxDataFileResponse(BaseModel):
     qcrbox_file_id: str
     filename: str | None
+
+
+class QCrBoxDataset(BaseModel):
+    dataset_id: str
+    data_files: list[QCrBoxDataFile]
+
+    def to_response_model(self) -> "QCrBoxDatasetResponse":
+        return QCrBoxDatasetResponse(
+            dataset_id=self.dataset_id,
+            data_files=[f.to_response_model() for f in self.data_files],
+        )
+
+
+class QCrBoxDatasetResponse(BaseModel):
+    dataset_id: str
+    data_files: list[QCrBoxDataFileResponse]
