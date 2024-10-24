91223d66d5373688d8ab09590053bf907c2c5aae
Maximilian Albert <maximilian.albert@gmail.com>
2024-10-24 23:25:33 +0100

    Remove commented code

--- pyqcrbox/pyqcrbox/data_management/dummy_data_file_manager.py
+++ pyqcrbox/pyqcrbox/data_management/dummy_data_file_manager.py
@@ -22,13 +22,3 @@ class DummyDataFileManager(DataFileManager):
 
     async def _retrieve_from_object_store(self, bucket: str, key: str) -> bytes:
         return self.dummy_storage[bucket][key]
-
-    # async def import_local_file(self, file_path: str | Path, _qcrbox_file_id: str | None = None) -> str:
-    #     with open(file_path, "rb") as f:
-    #         return await self.import_bytes(f.read(), filename=file_path.name, _qcrbox_file_id=_qcrbox_file_id)
-
-    # async def get_datasets(self) -> list[QCrBoxDataset]:
-    #     return list(self.datasets.values())
-
-    # async def delete(self, qcrbox_file_id: str) -> None:
-    #     _ = self.dummy_storage.pop(qcrbox_file_id, None)
