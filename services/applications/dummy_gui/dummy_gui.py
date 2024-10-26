425d756a739cb9a673d1f66cd8ad0802e94ce554
Maximilian Albert <maximilian.albert@gmail.com>
2024-10-26 12:25:26 +0100

    Add helper function to populate labels

--- services/applications/dummy_gui/dummy_gui.py
+++ services/applications/dummy_gui/dummy_gui.py
@@ -6,24 +6,31 @@ from pathlib import Path
 from tkinter import ttk
 
 
+def add_labels(frame, input_args):
+    try:
+        input_filepath = Path(input_args[0])
+        input_filename = input_filepath.name
+        ttk.Label(frame, text=f"Input file specified: {input_filename!r}\n", justify=tk.LEFT).pack(fill=tk.X)
+        if not input_filepath.exists():
+            ttk.Label(frame, text=f"Error: input file {input_filename!r} does not exist", justify=tk.LEFT).pack(
+                fill=tk.X
+            )
+    except IndexError:
+        ttk.Label(frame, text="No input file specified.").pack()
+
+
 def main():
     root = tk.Tk()
     root.title("Dummy GUI Application")
-    root.minsize(400, 200)
-    frm = ttk.Frame(root, padding=10)
-    frm.grid()
+    root.minsize(400, 0)
+    frame = ttk.Frame(root, padding=10)
+    frame.grid()
 
-    try:
-        input_filepath = Path(sys.argv[1])
-        input_filename = input_filepath.name
-        ttk.Label(frm, text=f"Input file specified: {input_filename!r}\n", justify=tk.LEFT).pack(fill=tk.X)
-        if not input_filepath.exists():
-            ttk.Label(frm, text=f"Error: input file {input_filename!r} does not exist", justify=tk.LEFT).pack(fill=tk.X)
-    except IndexError:
-        ttk.Label(frm, text="No input file specified.").pack()
+    input_args = sys.argv[1:]
+    add_labels(frame, input_args)
 
-    ttk.Label(frm, text="").pack()
-    ttk.Button(frm, text="Quit", command=root.destroy).pack()
+    ttk.Button(frame, text="Quit", command=root.destroy).pack(pady=(20, 10))
+
     root.mainloop()
 
 
