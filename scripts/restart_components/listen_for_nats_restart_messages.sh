d689bd0e47f32d1119304e8dada4ac84a561e504
Maximilian Albert <maximilian.albert@gmail.com>
2024-11-07 12:02:08 +0000

    Run restart script when a new NATS message is received

--- scripts/restart_components/listen_for_nats_restart_messages.sh
+++ scripts/restart_components/listen_for_nats_restart_messages.sh
@@ -3,10 +3,30 @@
 set -euo pipefail
 
 export QCRBOX_REPO=${QCRBOX_REPO:-"$(git rev-parse --show-toplevel)"}
+export SCRIPT_DIR="${QCRBOX_REPO}/scripts/restart_components"
+export NATS_MSG_DUMP_DIR="${SCRIPT_DIR}/restart_messages/"
+export NATS_SUBJECT="restart-qcrbox-containers"
+
+
+listen_to_nats_messages() {
+    echo "Listening to messages on NATS subject: '${NATS_SUBJECT}'"
+    echo "Messages will be dumped in the directory: ${NATS_MSG_DUMP_DIR}"
+    nats sub --dump=${NATS_MSG_DUMP_DIR} ${NATS_SUBJECT} &
+}
+
+
+restart_components_upon_new_message() {
+    while true; do
+      echo "================================================"
+      echo "[DDD] Beginning of while loop"
+      ls -d ${NATS_MSG_DUMP_DIR}/* | entr -p -d ./restart_components.sh
+      sleep 1
+    done
+}
 
 
 main() {
-    nats subscribe "restart-qcrbox-containers" | \
-        tee -a ${QCRBOX_REPO}/scripts/restart_components/nats_restart_messages.txt
+    listen_to_nats_messages
+    restart_components_upon_new_message
 }
 main
