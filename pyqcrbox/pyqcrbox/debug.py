6e9e6dff2dbb24961e03d4fbf23cf88e7856b790
Edward J. Parkinson <e.parkinson@soton.ac.uk>
2025-04-14 15:39:25 +0100

    update trace logging

--- pyqcrbox/pyqcrbox/debug.py
+++ pyqcrbox/pyqcrbox/debug.py
@@ -8,15 +8,15 @@ from pyqcrbox import logger
 _global_count = itertools.count()  # Thread-safe unique ID generator
 
 
-def _log_enter(id: int, func_name: str) -> None:
-    logger.debug(f"[Enter {id}] {func_name}")
+def _log_enter(call_id: int, func_name: str) -> None:
+    logger.debug(f"[EEL:Enter {call_id}] {func_name}")
 
 
-def _log_exit(id: int, full_name: str) -> None:
-    logger.debug(f"[Exit  {id}] {full_name}")
+def _log_exit(call_id: int, full_name: str) -> None:
+    logger.debug(f"[EEL:Exit {call_id}] {full_name}")
 
 
-def log_trace(func: Callable) -> Callable:
+def log_entry_exit(func: Callable) -> Callable:
     """Log entry and exit for a function.
 
     Both synchronous and asynchronous functions are supported.
@@ -35,28 +35,28 @@ def log_trace(func: Callable) -> Callable:
     def _get_func_name(args):
         func_name = func.__name__
         module_name = func.__module__
-        class_name = f"{args[0].__class__.__name__}." if args and hasattr(args[0], "__class__") else ""
+        class_name = f"{args[0].__class__.__name__}" if args and hasattr(args[0], "__class__") else ""
         return f"{module_name}.{class_name}::{func_name}"
 
     @wraps(func)
     async def _async_wrapper(*args, **kwargs):
-        count = next(_global_count)
-        full_name = _get_func_name(args)
+        call_id = next(_global_count)
+        func_name = _get_func_name(args)
 
-        _log_enter(count, full_name)
+        _log_enter(call_id, func_name)
         result = await func(*args, **kwargs)
-        _log_exit(count, full_name)
+        _log_exit(call_id, func_name)
 
         return result
 
     @wraps(func)
     def _sync_wrapper(*args, **kwargs):
-        count = next(_global_count)
-        function_name = _get_func_name(args)
+        call_id = next(_global_count)
+        func_name = _get_func_name(args)
 
-        _log_enter(count, function_name)
+        _log_enter(call_id, func_name)
         result = func(*args, **kwargs)
-        _log_exit(count, function_name)
+        _log_exit(call_id, func_name)
 
         return result
 
