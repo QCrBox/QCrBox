06c1be998c3ef5e610d33da0c2eedd35d206f62a
Edward J. Parkinson <e.parkinson@soton.ac.uk>
2025-04-29 15:40:59 +0100

    add script to remove all datasets from the QCrBox registry

---
+++ scripts/remove-datasets.py
@@ -0,0 +1,16 @@
+"""Remove all datasets from the QCrBox registry.
+"""
+
+import requests
+from loguru import logger
+
+base_url = "http://127.0.0.1:11000/api"
+datasets = requests.get(f"{base_url}/datasets").json()["data"]["datasets"]
+logger.info("Datasets before deletion:", datasets)
+
+dataset_ids = [dataset["dataset_id"] for dataset in datasets]
+for dataset_id in dataset_ids:
+    requests.delete(f"{base_url}/datasets/delete/{dataset_id}")
+
+datasets = requests.get(f"{base_url}/datasets").json()
+logger.info(f"Datasets after delection: {datasets}")
