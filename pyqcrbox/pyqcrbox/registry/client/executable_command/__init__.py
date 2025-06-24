3c842bb2bbf64c1a5b7b9579a6728c57916498a4
Edward J. Parkinson <e.parkinson@soton.ac.uk>
2025-06-24 11:55:10 +0100

    Remove deprecated InteractiveCmd

--- pyqcrbox/pyqcrbox/registry/client/executable_command/__init__.py
+++ pyqcrbox/pyqcrbox/registry/client/executable_command/__init__.py
@@ -1,4 +1,5 @@
 from .base_command import BaseCommand
 from .cli_command import CLICommand
 from .executable_command import ExecutableCommand
+from .interactive_session import InteractiveSession
 from .python_callable import PythonCallable
