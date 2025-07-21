04f6882634f98685213fb007078044569b8ad18c
Edward J. Parkinson <e.parkinson@soton.ac.uk>
2025-07-21 13:13:22 +0100

    add pytest to github CI

---
+++ .github/scripts/launch_qcb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <QCrBox directory>"
+    exit 1
+fi
+
+if [ ! -d "$1" ]; then
+    echo "Error: '$1' is not a valid directory."
+    exit 1
+fi
+
+cd "$1" || exit 1
+
+echo "Installing Python dependencies for QCrBox"
+pip install --upgrade uv
+uv pip install --system -r ./pyqcrbox/requirements-dev.txt
+uv pip install --system pyqcrbox@./pyqcrbox
+
+echo "Brining up QCrBox registry and test applications"
+docker container prune -f && docker network prune -f
+qcb up --test-only
