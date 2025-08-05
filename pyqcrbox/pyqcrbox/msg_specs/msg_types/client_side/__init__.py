f6a112311169310277c492b05c1073d9933c55ae
Edward J. Parkinson <e.parkinson@soton.ac.uk>
2025-08-05 14:35:36 +0100

    update init

--- pyqcrbox/pyqcrbox/msg_specs/msg_types/client_side/__init__.py
+++ pyqcrbox/pyqcrbox/msg_specs/msg_types/client_side/__init__.py
@@ -1,6 +1,6 @@
 from .command_execution_request import *
 from .command_invocation_client_response import *
 from .command_invocation_request import *
-from .end_command import *
+from .stop_calculation import *
 from .execute_command import *
 from .get_calculation_status import *
