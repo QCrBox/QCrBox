cfbc94b6990cf1d38979b2112b7f93447b061a2b
Edward J. Parkinson <e.parkinson@soton.ac.uk>
2025-08-21 12:59:42 +0100

    update crystal-explorer application: rename parameters and remove redundant code/comments

--- services/applications/crystal_explorer/configure_crystal_explorer.py
+++ services/applications/crystal_explorer/configure_crystal_explorer.py
@@ -1,18 +1,7 @@
-from pathlib import Path
-
-from qcrboxtools.cif.cif2cif import cif_file_to_specific_by_yml
-
 from pyqcrbox import sql_models
 from pyqcrbox.registry.client import QCrBoxClient
 
 YAML_PATH = "./config_crystal_explorer.yaml"
-
-
-def prepare__interactive(input_cif_path, work_cif_path):
-    input_cif_path = Path(input_cif_path)
-    work_cif_path = Path(work_cif_path)
-
-    cif_file_to_specific_by_yml(input_cif_path, work_cif_path, YAML_PATH, "interactive", "input_cif_path")
 
 
 if __name__ == "__main__":
