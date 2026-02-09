6f16162622f33056f15f28b0bed562239c0f40d2
Niolon <panikruth@gmail.com>
2026-02-09 14:00:17 +0000

    Fix Wine text rendering issues

--- services/applications/mopro/run_once.sh
+++ services/applications/mopro/run_once.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Fonts are already extracted and available in Wine prefix from Dockerfile build
+# Enable RGB subpixel font smoothing to fix missing vertical strokes
+wine reg add "HKCU\\Control Panel\\Desktop" /v FontSmoothing /t REG_SZ /d 2 /f 2>/dev/null || true
+wine reg add "HKCU\\Control Panel\\Desktop" /v FontSmoothingType /t REG_DWORD /d 2 /f 2>/dev/null || true
+
 wine $MOPRO_GUI_PATH &
 
 sleep 2
